e66610f232b176f27585cfc5f8ef67058168ea03
Felipe Alves <felipejean2002@gmail.com>
2022-06-13 03:25:50 -0300

    feat: Creating base system for rendering model

--- src/pages/EditorPage/Viewport/index.tsx
+++ src/pages/EditorPage/Viewport/index.tsx
@@ -1,9 +1,18 @@
+import React from "react";
 import { Box } from "@mui/material";
 
 function Viewport() {
+    const viewerElement = React.useRef<HTMLElement>();
+    
+    React.useEffect(() => {
+        if (viewerElement.current) {
+            // TODO: Render 3D model
+            viewerElement.current.innerHTML = "<h1>Hello, world!</h1>";
+        }
+    }, [viewerElement]);
+    
     return (
-        <Box className="Viewport" component="main">
-            
+        <Box ref={viewerElement} component="section" className="Editor-viewport">
         </Box>
     );
 }
