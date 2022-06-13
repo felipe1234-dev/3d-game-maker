04c857368917f90cc2466ed4af6814a8bbfb0690
Felipe Alves <felipejean2002@gmail.com>
2022-06-13 03:14:06 -0300

    refactor: Applying semantic HTML

--- src/pages/EditorPage/index.tsx
+++ src/pages/EditorPage/index.tsx
@@ -1,11 +1,12 @@
+import { Box } from "@mui/material";
 import Viewport from "./Viewport";
 import "@local/styles/pages/EditorPage.scss";
 
 function EditorPage() {
     return (
-        <>
+        <Box component="main" className="Editor">
             <Viewport />
-        </>
+        </Box>
     );
 }
 
