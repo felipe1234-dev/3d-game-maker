25ed09515aeb6b54c1a8c9fd513af1216a4aa215
Felipe Alves <felipejean2002@gmail.com>
2022-06-14 19:48:11 -0300

    feat: Rendering sample scene by using ScreenContext with Game.Screen class

--- src/pages/EditorPage/Viewport/index.tsx
+++ src/pages/EditorPage/Viewport/index.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { Box } from "@mui/material";
+import * as THREE from "three";
+
+import * as Game from "@local/classes/Game";
+import { ScreenContext } from "@local/contexts";
 
 function Viewport() {
-    const viewerElement = React.useRef<HTMLElement>();
+    const viewportElement = React.useRef<HTMLElement>();
+    const { screen, setScreen } = React.useContext(ScreenContext);
     
     React.useEffect(() => {
-        if (viewerElement.current) {
-            // TODO: Render 3D model
-            viewerElement.current.innerHTML = "<h1>Hello, world!</h1>";
+        const container = viewportElement.current;
+        
+        if (container && container.innerHTML.length === 0) {
+            const scene = new THREE.Scene();
+            
+            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			const cube = new THREE.Mesh( geometry, material );
+			
+            scene.add(cube);
+                 
+            const screen = new Game.Screen(container);
+            
+            screen.scene = scene;
+            screen.renderer = new THREE.WebGLRenderer();
+            
+            screen.animate();
+            
+            setScreen(screen);
         }
-    }, [viewerElement]);
+    }, [viewportElement, screen]);
     
     return (
-        <Box ref={viewerElement} component="section" className="Editor-viewport">
+        <Box ref={viewportElement} component="section" className="Editor-viewport">
         </Box>
     );
 }
