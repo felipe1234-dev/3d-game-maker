1acdc25db94bcc4f16a2d4df071acb1e6857ec6c
Felipe Alves <felipejean2002@gmail.com>
2022-06-14 19:51:45 -0300

    feat: Adding the master provider that encompasses all the other providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MasterProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -8,6 +9,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <App />
+        <MasterProvider>
+            <App />
+        </MasterProvider>
     </React.StrictMode>
 );
