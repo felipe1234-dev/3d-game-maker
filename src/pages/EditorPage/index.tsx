44fe9ef0c3e7f87cee6912bb99a762693e05b5bf
Felipe Alves <felipejean2002@gmail.com>
2022-06-15 13:23:07 -0300

    feat: Importing Menubar

--- src/pages/EditorPage/index.tsx
+++ src/pages/EditorPage/index.tsx
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material";
+
+import Menubar from "./Menubar";
 import Viewport from "./Viewport";
+
 import "@local/styles/pages/EditorPage.scss";
 
 function EditorPage() {
     return (
         <Box component="main" className="Editor">
+            <Menubar /> 
             <Viewport />
         </Box>
     );
