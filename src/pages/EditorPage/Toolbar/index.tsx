9123f2311f1602dcd0a51bc5b7076c641e6fc114
Felipe Alves <felipejean2002@gmail.com>
2022-06-16 16:21:25 -0300

    feat: Creating zoom function

--- src/pages/EditorPage/Toolbar/index.tsx
+++ src/pages/EditorPage/Toolbar/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box, IconButton } from "@mui/material";
 import {
     AddRounded as ZoomInIcon,
@@ -5,21 +6,37 @@ import {
 } from "@mui/icons-material";
 
 import { Pressable } from "@local/components";
+import { ScreenContext } from "@local/contexts";
 
 function Toolbar() {
+    const { screen } = React.useContext(ScreenContext);
+    
+    const onZoom = (type: "in" | "out") => {
+        if (screen) {
+            const wheelEvent = new WheelEvent("wheel", {
+                deltaY: type === "in" ? -240 : +240,
+                bubbles: true, 
+                cancelable: true
+            });
+            screen.canvas.dispatchEvent(wheelEvent);
+        }
+    }
+    
     return (
         <Box className="Editor-toolbar" component="aside">
             <Pressable 
                 component={IconButton}
                 aria-label="Zoom In"
-                onMousePress={() => console.log("Zoom in")}
+                onMousePress={() => onZoom("in")}
+                ms={50}
             >
                 <ZoomInIcon />
             </Pressable>
             <Pressable 
                 component={IconButton}
                 aria-label="Zoom Out"
-                onMousePress={() => console.log("Zoom out")}
+                onMousePress={() => onZoom("out")}
+                ms={50}
             >
                 <ZoomOutIcon />
             </Pressable>
