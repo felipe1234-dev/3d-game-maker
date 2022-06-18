228a4b92d172997c1d22953997cb623ef7b375c3
Felipe Alves <felipejean2002@gmail.com>
2022-06-18 15:15:30 -0300

    fix: Using Game and Editor providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { EditorPage } from "@local/pages";
+import { GameProvider, EditorProvider } from "@local/contexts";
+
 import "./styles/reset.css";
 import "./styles/base.css";
 
 function App() {
     return (
-        <>
-            <EditorPage />
-        </>
+        <GameProvider>
+            <EditorProvider>
+                <EditorPage />
+            </EditorProvider>
+        </GameProvider>
     );
 }
 
