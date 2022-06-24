e80cea82837b0cba012dfdbfa9ef55e0adca8df2
Felipe Alves <felipejean2002@gmail.com>
2022-06-24 11:40:21 -0300

    feat: Creating system to expand and unexpand sidebar

--- src/pages/EditorPage/index.tsx
+++ src/pages/EditorPage/index.tsx
@@ -1,17 +1,37 @@
+import React from "react";
 import { Box } from "@mui/material";
 
 import Menubar from "./Menubar";
 import Viewport from "./Viewport";
 import Toolbar from "./Toolbar";
+import Sidebar from "./Sidebar";
 
 import "@local/styles/pages/EditorPage.scss";
 
 function EditorPage() {
+    const [open, setOpen] = React.useState<boolean>(true);
+    const sidebarIsExpanded = open;
+    const mainIsExpanded = !sidebarIsExpanded;
+    
     return (
-        <Box component="main" className="Editor">
-            <Menubar /> 
-            <Viewport />
-            <Toolbar />
+        <Box component="div" className="Editor">
+            <Box 
+                component="main" 
+                className="Editor-main"
+                aria-expanded={mainIsExpanded}
+            >
+                <Menubar
+                    open={open}
+                    setOpen={setOpen}
+                /> 
+                <Viewport />
+                <Toolbar />
+            </Box>
+            <Sidebar
+                aria-expanded={sidebarIsExpanded}
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+            />
         </Box>
     );
 }
