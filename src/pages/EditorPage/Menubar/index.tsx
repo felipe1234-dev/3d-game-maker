f1829feb1fe3766fb9fbfe269d30d892c796e777
Felipe Alves <felipejean2002@gmail.com>
2022-06-24 11:35:10 -0300

    feat: Creating button to set sidebar visible

--- src/pages/EditorPage/Menubar/index.tsx
+++ src/pages/EditorPage/Menubar/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { 
+    AppBar, 
+    Box, 
+    Toolbar, 
+    IconButton 
+} from "@mui/material";
+import { ChevronLeft as ArrowLeftIcon } from "@mui/icons-material";
 
 import FileItem from "./FileItem";
 import EditItem from "./EditItem";
 import AddItem from "./AddItem";
 
-function Menubar() {
+interface MenubarProps {
+    open: boolean, // If sidebar is open
+    setOpen: (open: boolean) => void
+}
+
+function Menubar({ open, setOpen }: MenubarProps) {
     return (
         <AppBar className="Editor-menubar" component="header">
             <Toolbar>
@@ -13,6 +24,14 @@ function Menubar() {
                 <EditItem />
                 <AddItem />
                 <Box flexGrow={1} />
+                {!open && (
+                    <IconButton
+                        onClick={() => setOpen(true)}
+                        aria-label="open sidebar"
+                    >
+                        <ArrowLeftIcon />
+                    </IconButton>
+                )}
             </Toolbar>
         </AppBar>
     );
