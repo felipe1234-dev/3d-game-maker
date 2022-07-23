03b080680094552318982872b11813d8b10c71e9
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-22 21:08:10 -0300

    add: Export new fields

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,7 +1,11 @@
 export interface FieldProps {
-    attribute: string,
-    label: string
+    scope: "object" | "object.geometry" | "object.material",
+    labels: Array<string>,
+    attributes: Array<string>,
+    step?: number
 }
 
 export { default as CheckboxField } from "./CheckboxField";
 export { default as ColorField } from "./ColorField";
+export { default as MultiNumberField } from "./MultiNumberField";
+export { default as SingleNumberField } from "./SingleNumberField";
