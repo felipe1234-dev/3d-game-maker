a8816c229ad4bf032c19183f341aa153341bc2a3
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-22 21:08:53 -0300

    add: Render the object's fields

--- src/modals/EditObject/index.tsx
+++ src/modals/EditObject/index.tsx
@@ -10,6 +10,9 @@ import {
 } from "@mui/material";
 import { TransitionProps } from "@mui/material/transitions";
 import { useNavigate } from "react-router-dom";
+import { EditorContext } from "@local/contexts";
+import { objectList, objectFields } from "@local/consts";
+import { Helper } from "@local/components";
 
 const Transition = (
     React.forwardRef(
@@ -29,6 +32,13 @@ const Transition = (
 );
 
 function EditObject() {
+    const editor = React.useContext(EditorContext);
+    const object = editor?.transformControls.object || null; 
+    const abstract: typeof objectList[string] = !object ? {
+        label: "Edit object",
+        keys: []
+    } : objectList[object.constructor.name];
+
     const navigate = useNavigate();
     
     const goBack = () => {
@@ -48,12 +58,36 @@ function EditObject() {
             aria-describedby="alert-dialog-slide-description"
         >
             <DialogTitle>
-                Edit object
+                {abstract.label}
             </DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-slide-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
+                    {abstract.keys.map(key => {
+                        const field = objectFields.find(field => (
+                            field.key === key
+                        )); 
+
+                        if (!field) {
+                            return <></>;
+                        }
+
+                        const { 
+                            key: fieldKey, 
+                            Component: Field, 
+                            helpText, 
+                            ...props 
+                        } = field;
+                        return (
+                            <Helper 
+                                key={fieldKey} 
+                                text={helpText} 
+                                placement="right" 
+                                arrow
+                            >
+                                <Field scope="object" {...props} />
+                            </Helper>
+                        );
+                    })}
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
