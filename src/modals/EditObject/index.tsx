bec0aced0848340c9b6801e0966dccc0c3cd9e68
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-24 01:12:40 -0300

    refactor & feat: Use the new Modal component and make it draggable

--- src/modals/EditObject/index.tsx
+++ src/modals/EditObject/index.tsx
@@ -1,39 +1,10 @@
 import React from "react";
-import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Button,
-    DialogContentText,
-    Slide
-} from "@mui/material";
-import { TransitionProps } from "@mui/material/transitions";
-import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 import { EditorContext } from "@local/contexts";
 import { objectList, objectFields } from "@local/consts";
-import { Helper } from "@local/components";
+import { Modal, Helper } from "@local/components";
 
-const Transition = (
-    React.forwardRef(
-        function Transition(
-            props: TransitionProps & { children: React.ReactElement<any, any> },
-            ref: React.Ref<unknown>
-        ) {
-            return (
-                <Slide 
-                    ref={ref}
-                    direction="up"  
-                    {...props} 
-                />
-            );
-        }
-    )
-);
-
-function EditObject() {
-    const [open, setOpen] = React.useState<boolean>(true);
-
+function EditObjectModal() {
     const editor = React.useContext(EditorContext);
     const object = editor?.transformControls.object || null; 
     const abstract: typeof objectList[string] = !object ? {
@@ -41,33 +12,15 @@ function EditObject() {
         keys: []
     } : objectList[object.constructor.name];
 
-    const navigate = useNavigate();
-    
-    const goBack = () => {
-        setOpen(false);
-
-        setTimeout(() => {
-            navigate("/editor/", {
-                state: { 
-                    useLoader: false 
-                }
-            });
-        }, 1000);
-    }
-    
     return (
-        <Dialog
-            open={open}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={goBack}
-            aria-describedby="alert-dialog-slide-description"
-        >
-            <DialogTitle>
-                {abstract.label} {object?.name}
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
+        <Modal 
+            height={500}
+            width={400}
+            placement="bottom-left"
+            draggable
+            header={`${abstract.label} ${object?.name || ""}`}
+            body={(
+                <>
                     {abstract.keys.map(key => {
                         const field = objectFields.find(field => (
                             field.key === key
@@ -94,14 +47,10 @@ function EditObject() {
                             </Helper>
                         );
                     })}
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button>Save</Button>
-                <Button>Agree</Button>
-            </DialogActions>
-        </Dialog>
+                </>
+            )}
+        />
     );
 }
 
-export default EditObject;
+export default EditObjectModal;
