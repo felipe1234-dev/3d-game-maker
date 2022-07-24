16627cfe0dff4a111b663df9471c5004acf29436
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-24 01:19:13 -0300

    chore: Install react-draggable and react-resizable

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,8 @@
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
+declare module 'react-resizable';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
