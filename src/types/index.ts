ab2c06c69ae4b55121cfdd2c79d9ab26ba55b6a9
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-31 15:26:03 -0300

    add: Types for the API

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,7 @@
+import { FieldPath, OrderByDirection, WhereFilterOp } from "firebase/firestore";
+
 export type Severity = "error" | "warning" | "info" | "success";
+
+export type WhereClasule = readonly [FieldPath | string, WhereFilterOp, unknown];
+
+export type OrderByClasule = readonly [FieldPath | string, OrderByDirection];
