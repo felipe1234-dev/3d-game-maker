3d44c2aae835f8e17bff7890803a56f84c451298
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-31 15:06:11 -0300

    change: Property changes

--- src/api/models/Media.model.ts
+++ src/api/models/Media.model.ts
@@ -1,23 +1,27 @@
 import BaseModel from "./BaseModel.model";
 
 class Media extends BaseModel {
+    public title: string;
     public description: string;
-    public folders: Array<string>;
-    
-    public alt: string;
-    public url: string;
 
+    public folders: Array<string>;
+    public mimeType: string;
+    public extension: string;
+
+    public url: string;
     public createdBy: string;
 
     constructor() {
         super();
 
+        this.title = "";
         this.description = "";
         this.folders = [];
 
-        this.alt = "";
-        this.url = "";
+        this.mimeType = "";
+        this.extension = "";
 
+        this.url = "";
         this.createdBy = "";
     }
 }
