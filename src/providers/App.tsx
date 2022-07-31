b1e8f6783e59e229b127f19234b65d5a845bbea2
Felipe Alves <felipe.alves@taugor.com.br>
2022-07-31 15:13:47 -0300

    add: Other providers

--- src/providers/App.tsx
+++ src/providers/App.tsx
@@ -1,13 +1,17 @@
 import { 
     AlertProvider, 
     GameProvider,
-    EditorProvider
+    EditorProvider,
+    FirebaseProvider,
+    UserProvider
 } from "@local/contexts";
 import { ProviderComposer } from "@local/components";
 
 function App(props: { children: React.ReactNode }) {
     return (
         <ProviderComposer providers={[
+            FirebaseProvider,
+            UserProvider,
             AlertProvider,
             GameProvider,
             EditorProvider
