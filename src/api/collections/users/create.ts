a222c566f9c49b6f27bf320f71ed4641fbc59347
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-06 15:20:03 -0300

    add: Function for creating users

---
+++ src/api/collections/users/create.ts
@@ -0,0 +1,29 @@
+import { setDoc, doc } from "firebase/firestore";
+import { User } from "@local/api/models";
+import { split, toAlert } from "@local/api/functions";
+import * as users from "./index";
+import { db } from "@local/api";
+import { FirebaseError } from "firebase/app";
+
+export default function create(props: User): Promise<User> {
+    return new Promise(async (resolve, reject) => {
+        const user = props;
+
+        user.tags.push(user.uid, user.email);
+        user.tags.push(...split(user.firstName));
+        user.tags.push(...split(user.lastName));
+        user.tags.push(
+            new Date(user.createdAt.seconds * 1000)
+                .toLocaleDateString()
+        );
+
+        try {
+            const docRef = doc(db, users.collectionName, user.uid);
+            await setDoc(docRef, { ...user });
+            
+            resolve(user);
+        } catch (error) {
+            reject(toAlert(error as FirebaseError));
+        }
+    });
+};
--- src/api/collections/users/index.ts
+++ src/api/collections/users/index.ts
@@ -1,2 +1,3 @@
 export const collectionName = "users";
 export { default as byUid } from "./byUid";
+export { default as create } from "./create";
