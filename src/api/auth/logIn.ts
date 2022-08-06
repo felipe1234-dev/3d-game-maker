90b14979ab9f7e9a5c05e7a17ecf106df0ea24ef
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-06 15:19:10 -0300

    refactor: Make code more readable

--- src/api/auth/logIn.ts
+++ src/api/auth/logIn.ts
@@ -1,23 +1,20 @@
-// Firebase
 import { FirebaseError } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-// Local
 import { toAlert } from "../functions";
 
 export default function logIn(email: string, password: string): Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const auth = getAuth();
         
-        signInWithEmailAndPassword(auth, email, password) 
-            .then((resp: any) => {
-                sessionStorage.setItem("Auth Token", resp._tokenResponse.refreshToken);
-                sessionStorage.setItem("Assign Date", (new Date().getTime()).toString());
-                
-                resolve();
-            })
-            .catch((error: FirebaseError) => (
-                reject(toAlert(error))
-            ));
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            
+            sessionStorage.setItem("Auth Token", userCredential.user.refreshToken);
+            sessionStorage.setItem("Assign Date", (new Date().getTime()).toString());
+            
+            resolve();
+        } catch (error) {
+            reject(toAlert(error as FirebaseError))
+        }
     });
 };
