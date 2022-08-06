f054c80237cb25d06fe0ec65d9078e5dd17ba90f
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-06 15:11:55 -0300

    add: Function for registering

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,3 +1,4 @@
 export { default as currentUser } from "./currentUser";
 export { default as logIn } from "./logIn";
 export { default as logOut } from "./logOut";
+export { default as register } from "./register";
---
+++ src/api/auth/register.ts
@@ -0,0 +1,47 @@
+import { Alert } from "@local/interfaces";
+import { FirebaseError } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import * as users from "../collections/users";
+import * as auth from "./index";
+
+import { toAlert } from "../functions";
+import { User } from "../models";
+
+export default function register(props: {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+}): Promise<User> {
+    return new Promise(async (resolve, reject) => {
+        const firebaseAuth = getAuth();
+        const {
+            firstName,
+            lastName,
+            email,
+            password
+        } = props;
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+            let user = new User();
+
+            user.uid = userCredential.user.uid;
+            user.firstName = firstName;
+            user.lastName = lastName;
+            user.email = email;
+
+            try {
+                user = await users.create(user);
+                await auth.logIn(email, password);
+                
+                resolve(user);
+            } catch (error) {
+                reject(error as Alert);
+            }
+        } catch (error) {
+            reject(toAlert(error as FirebaseError));
+        }
+    });
+};
