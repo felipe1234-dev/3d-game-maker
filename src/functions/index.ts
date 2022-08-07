4f3342e58f24850e97de66c65b9d866dacab0e03
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-07 19:19:11 -0300

    add: Export new functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,4 +3,6 @@ export { default as getProperty } from "./getProperty";
 export { default as isLocation } from "./isLocation";
 export { default as isRouteState } from "./isRouteState";
 export { default as setProperty } from "./setProperty";
+export { default as stringToAvatar } from "./stringToAvatar";
+export { default as stringToColor } from "./stringToColor";
 export { default as threeColorToHex } from "./threeColorToHex";
