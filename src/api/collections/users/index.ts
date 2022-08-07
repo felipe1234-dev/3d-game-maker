a4abee964847d37dfde47ae57bd73ed5ce593e41
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-07 00:38:57 -0300

    add: Function for updating users

--- src/api/collections/users/index.ts
+++ src/api/collections/users/index.ts
@@ -1,3 +1,4 @@
 export const collectionName = "users";
 export { default as byUid } from "./byUid";
 export { default as create } from "./create";
+export { default as update } from "./update";
---
+++ src/api/collections/users/update.ts
@@ -0,0 +1,63 @@
+import { FirebaseError } from "firebase/app";
+import { 
+    doc, 
+    Firestore,
+    getDoc,
+    updateDoc
+} from "firebase/firestore";
+import { collectionName } from "./index";
+import * as auth from "@local/api/auth";
+import { toAlert } from "@local/api/functions";
+import { User } from "@local/api/models";
+import { db } from "@local/api";
+
+export default function update(uid: string, updates: Partial<User>): Promise<User> {
+    return new Promise(async (resolve, reject) => {
+        const docRef = doc(db, collectionName, uid);
+
+        // For safety
+        delete updates.uid;
+        delete updates.createdAt;
+
+        try {
+            const docSnap = await getDoc(docRef);
+            
+            if (docSnap.exists()) {
+                try {
+                    const authUser = await auth.currentUser();
+                    
+                    if (!!authUser) {
+                        if (authUser.uid === uid || authUser.admin) {
+                            try {
+                                await updateDoc(docRef, updates);
+                                resolve({ ...docSnap.data() } as User);
+                            } catch (error) {
+                                const err = error as FirebaseError;
+                                reject(toAlert(err));
+                            }
+                        } else {
+                            reject({
+                                severity: "error",
+                                message: "You're trying to update a user who isn't you. Only admins can update data from users other than them."
+                            });
+                        }
+                    } else {
+                        reject({
+                            severity: "error",
+                            message: "User not authenticated"
+                        });
+                    }
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                reject({
+                    severity: "error",
+                    message: "User doesn't exist"
+                });
+            }
+        } catch (error) {
+            reject(toAlert(error as FirebaseError));
+        }
+    });
+};
