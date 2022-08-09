bf8599a60caaf5378564838174363c470618b062
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-09 20:04:15 -0300

    fix: getLang now checks closest match

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -10,9 +10,26 @@ const i18n: {
     pt_BR
 }
 
-const getLang = () => (
-    window.location.href.replace(/^.+\/#\/(\w+)\/.+$/, "$1")
-);
+const getLang = () => {
+    const langInURL = window.location.href.replace(/^.+\/#\/(\w+)\/.+$/, "$1");
+    const fixedLang = langInURL.replace("-", "_").replace(/_(\w+)$/g, m => m.toUpperCase());
+
+    let trans = i18n[fixedLang];
+        
+    if (!trans) {
+        const closestMatch = Object.keys(i18n).find(langItem => (
+            langItem.includes(fixedLang) || fixedLang.includes(langItem)
+        ));
+            
+        if (closestMatch) {
+            return closestMatch;
+        } else {
+            return "en_US";
+        }
+    } else {
+        return fixedLang;
+    }
+};
 
 export default i18n;
 export { getLang };
