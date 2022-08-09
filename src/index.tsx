43dbe7d45c7e1fc9053aa1cf884538ac152a4918
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-09 20:13:10 -0300

    add Apply I18nProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,16 @@
-// Libs
 import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
-
-// Local components
+import { App as AppProvider } from "@local/providers";
 import App from "./App";
-
-// Providers
-import { App as AppProvider } from "./providers";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 
 root.render(
-    <AppProvider>
-        <Router>
+    <Router>
+        <AppProvider>
             <App />
-        </Router>
-    </AppProvider>
+        </AppProvider>
+    </Router>
 );
--- src/providers/App.tsx
+++ src/providers/App.tsx
@@ -1,9 +1,10 @@
-import { AlertProvider } from "@local/contexts";
+import { AlertProvider, I18nProvider } from "@local/contexts";
 import { ProviderComposer } from "@local/components";
 
 function App(props: { children: React.ReactNode }) {
     return (
         <ProviderComposer providers={[
+            I18nProvider,
             AlertProvider
         ]}>
             {props.children}
