6d0cbdc64f934d8f79ab258db6342f37a3c71037
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-12 20:56:00 -0300

    add: Home page container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
+import { Box } from "@mui/material";
 import Navbar from "./Navbar";
 import "@local/styles/pages/HomePage.scss";
 
 function HomePage() {
     return (
-        <>
+        <Box className="HomePage">
             <Navbar />
-        </>
+        </Box>
     );
 }
 
