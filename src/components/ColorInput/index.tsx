72b28871d7a78867aa7c72cd2b6e58226fbcbe1b
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-14 10:03:24 -0300

    add: New component

---
+++ src/components/ColorInput/index.tsx
@@ -0,0 +1,44 @@
+import { TextFieldProps } from "@material-ui/core";
+import ColorPicker from "material-ui-color-picker";
+import "@local/styles/components/ColorInput.scss";
+
+interface ColorInputProps extends Omit<TextFieldProps, "onChange"> {
+    value: string;
+    defaultValue?: string;
+    onChange: (color: string) => void;
+    convert?: string | number | symbol | undefined;
+    hintText?: string;
+    floatingLabelText?: string;
+    showPicker?: boolean;
+    internalValue?: string;
+    setShowPicker?: (open: boolean) => void;
+    setValue?: (value: string) => void;
+}
+
+function ColorInput(props: ColorInputProps) {
+    const { className, value } = props;
+    
+    return (
+        <ColorPicker
+            className={`ColorInput ${className ?? ""}`.trim()}
+            {...props}
+            value={value}
+            InputProps={{
+                value,
+                startAdornment: (
+                    <div style={{
+                        minHeight: 30,
+                        minWidth: 30,
+                        borderRadius: 8,
+                        border: '2px solid #e9ebf0',
+                        marginRight: 8,
+                        backgroundColor: value,
+                    }} />
+                ),
+            }}
+        />
+    );
+}
+
+export default ColorInput;
+export type { ColorInputProps };
