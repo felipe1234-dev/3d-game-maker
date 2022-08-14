4dcfe68aa7a3b7bbc84101c7cb2a7dd62d94975e
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-14 10:04:57 -0300

    refactor: import/export order

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,26 @@
 export { default as AlertMessage } from "./AlertMessage";
 export type { AlertMessageProps } from "./AlertMessage";
 
+export { default as ColorInput } from "./ColorInput";
+export type { ColorInputProps } from "./ColorInput";
+
+export { default as Composer } from "./Composer";
+export type { ComposerProps } from "./Composer";
+
 export { default as Helper } from "./Helper";
 export type { HelperProps } from "./Helper";
 
 export { default as Modal } from "./Modal";
 export type { ModalProps } from "./Modal";
 
+export { default as PageLoader } from "./PageLoader";
+export type { PageLoaderProps } from "./PageLoader";
+
 export { default as Pressable } from "./Pressable";
 export type { PressableProps } from "./Pressable";
 
 export { default as ProfileImage } from "./ProfileImage";
 export type { ProfileImageProps } from "./ProfileImage";
-
-export { default as Composer } from "./Composer";
-export type { ComposerProps } from "./Composer";
-
-export { default as PageLoader } from "./PageLoader";
-export type { PageLoaderProps } from "./PageLoader";
 
 export { default as RequireAuth } from "./RequireAuth";
 export type { RequireAuthProps } from "./RequireAuth";
