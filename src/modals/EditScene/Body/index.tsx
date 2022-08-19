faa1af5b3aff16d3f7e4f6294a6c6214c5154cfe
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-19 19:04:56 -0300

    feat: Change sene name

--- src/modals/EditScene/Body/index.tsx
+++ src/modals/EditScene/Body/index.tsx
@@ -1,14 +1,39 @@
-import { Divider } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { Divider, TextField } from "@mui/material";
+import { GameContext, I18nContext } from "@local/contexts";
+
 import Background from "./Background";
 import Environment from "./Environment";
 
 function Body() {
+    const game = useContext(GameContext);
+    const i18n = useContext(I18nContext);
+    const scope = "modals.editScene.body.index.";
+
+    const [sceneName, setSceneName] = useState<string>("");
+
+    useEffect(() => {
+        if (!game) return;
+        setSceneName(game.currentScene.name);
+    }, [game, game?.currentScene]);
+
+    useEffect(() => {
+        if (!game) return;
+        game.currentScene.name = sceneName;
+    }, [sceneName]);
+
     return (
-        <>
+        <div style={{ paddingTop: 15 }}>
+            <TextField
+                label={i18n.get(scope + "sceneName")}
+                onChange={evt => setSceneName(evt.target.value)}
+                value={sceneName}
+            />
+            <Divider />
             <Background />
             <Divider />
             <Environment />
-        </>
+        </div>
     );
 }
 
