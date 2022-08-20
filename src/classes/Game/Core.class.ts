623d76d02e001154099a5d91c8f01762e5750848
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-19 23:13:14 -0300

    add: Stages and default scene

--- src/classes/Game/Core.class.ts
+++ src/classes/Game/Core.class.ts
@@ -1,30 +1,34 @@
-import * as Game from "./index";
+import * as THREE from "three";
+import { Game } from "..";
 
 class Core {
-    public readonly scenes: Array<Game.Scene>;
+    public stages: Game.Stage[];
+    public scenes: Game.Scene[];
+
     public currentScene: Game.Scene;
-    
-    constructor(scenes: Array<Game.Scene>) {
-        this.scenes = scenes;
-        this.currentScene = scenes[0];
-    }
-    
-    public addScene(scene: Game.Scene): void {
-        this.scenes.push(scene);
-    }
-    
-    public goToScene(name: string): void {
-        const foundScene = this.scenes.find((scene) => scene.name === name);
-        this.currentScene = foundScene ?? this.currentScene;
-    }
-    
-    public getScene(name: string): Game.Scene | undefined {
-        return this.scenes.find((scene) => scene.name === name);
-    }
-    
-    public setCurrentScene(name: string): void {
-        const scene = this.getScene(name);
-        this.currentScene = scene ?? this.currentScene;
+    public currentStage: Game.Stage;
+
+    constructor(scenes?: Game.Scene[], stages?: Game.Stage[]) {
+        const stage = new Game.Stage("Stage 1", this);
+        const scene = new Game.Scene("Scene 1", stage, this);
+        
+        scene.background = Game.Scene.DEFAULT_BACKGROUND;
+        scene.environment = Game.Scene.DEFAULT_ENVIRONMENT;
+        scene.fog = Game.Scene.DEFAULT_FOG;
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        
+        scene.add(cube);
+        
+        this.stages = stages || [ stage ];
+        this.scenes = scenes || [ scene ];
+
+        this.currentScene = this.scenes[0] || scene;
+        this.currentStage = this.scenes[0]?.stage || stage;
+
+        stage.addScene(scene);
     }
 }
 
