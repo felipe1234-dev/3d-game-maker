f9e7563a8c6c840660d84ad8189b71f303a6a563
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-19 23:13:42 -0300

    add: Parent stage and default props

--- src/classes/Game/Scene.class.ts
+++ src/classes/Game/Scene.class.ts
@@ -1,9 +1,23 @@
 import * as THREE from "three";
+import { Game } from "..";
 
 class Scene extends THREE.Scene {
-    constructor(name: string) {
+    static DEFAULT_BACKGROUND: THREE.Color = new THREE.Color("#444");
+    static DEFAULT_ENVIRONMENT: null = null;
+    static DEFAULT_FOG: null = null;
+
+    public stage: Game.Stage;
+    public game: Game.Core;
+
+    constructor(name: string, stage: Game.Stage, game: Game.Core) {
         super();
         this.name = name;
+        this.stage = stage;
+        this.game = game;
+    }
+
+    public select(): void {
+        this.game.currentScene = this;
     }
 }
 
