da63439c5263e0ddbc0d347478340fe688e9bc23
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-20 18:31:38 -0300

    change: Default game now has two stages

--- src/classes/Game/Core.class.ts
+++ src/classes/Game/Core.class.ts
@@ -9,26 +9,40 @@ class Core {
     public currentStage: Game.Stage;
 
     constructor(scenes?: Game.Scene[], stages?: Game.Stage[]) {
-        const stage = new Game.Stage("Stage 1", this);
-        const scene = new Game.Scene("Scene 1", stage, this);
-        
-        scene.background = Game.Scene.DEFAULT_BACKGROUND;
-        scene.environment = Game.Scene.DEFAULT_ENVIRONMENT;
-        scene.fog = Game.Scene.DEFAULT_FOG;
+        const stage1 = new Game.Stage("Stage 1", this);
+        const scene1 = new Game.Scene("Scene 1", this);
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const stage2 = new Game.Stage("Stage 2", this);
+        const scene2 = new Game.Scene("Scene 2", this);
+
+        [scene1, scene2].forEach(scene => {
+            scene.background = Game.Scene.DEFAULT_BACKGROUND;
+            scene.environment = Game.Scene.DEFAULT_ENVIRONMENT;
+            scene.fog = Game.Scene.DEFAULT_FOG;
+        });
+
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        
-        scene.add(cube);
-        
-        this.stages = stages || [ stage ];
-        this.scenes = scenes || [ scene ];
 
-        this.currentScene = this.scenes[0] || scene;
-        this.currentStage = this.scenes[0]?.stage || stage;
+        const box = new THREE.BoxGeometry(1, 1, 1);
+        const cube = new THREE.Mesh(box, material);
 
-        stage.addScene(scene);
+        const ball =  new THREE.SphereGeometry(1, 50, 50);
+        const sphere = new THREE.Mesh(ball, material);
+
+        scene1.add(cube);
+        scene2.add(sphere);
+        
+        scene1.stage = stage1;
+        scene2.stage = stage2;
+
+        stage1.scenes = [scene1];
+        stage2.scenes = [scene2];
+
+        this.stages = stages || [ stage1, stage2 ];
+        this.scenes = scenes || [ scene1, scene2 ];
+
+        this.currentScene = this.scenes[0] || scene1;
+        this.currentStage = this.scenes[0]?.stage || stage1;
     }
 }
 
