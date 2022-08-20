2dbdbce2fc37ce75cabcf6371f0bf93a87d36508
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-19 23:17:42 -0300

    change: Game class already has a default scene and stage if not provided

--- src/contexts/Game.tsx
+++ src/contexts/Game.tsx
@@ -9,16 +9,7 @@ function GameProvider(props: { children: React.ReactNode }) {
     const [game, setGame] = React.useState<Game.Core>();
 
     React.useEffect(() => {
-        const scene = new Game.Scene("Stage 1");
-            
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        
-        scene.add(cube);
-        
-        const obj = new Game.Core([ scene ]);
-        
+        const obj = new Game.Core();
         setGame(obj);
     }, []);
     
