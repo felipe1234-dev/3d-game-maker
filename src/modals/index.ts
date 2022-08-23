505aab6554d22ec39a4526c3bfe6dc5c233e28a9
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-23 12:23:17 -0300

    add: Route for adding objects

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -1,3 +1,5 @@
 export { default as EditObjectModal } from "./Editor/Object/Edit";
+export { default as AddObjectModal } from "./Editor/Object/Add";
+
 export { default as EditSceneModal } from "./Editor/Scene/Edit";
 export { default as EditSceneTreeModal } from "./Editor/SceneTree/Edit";
