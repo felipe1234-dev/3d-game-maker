3a9e3268bc928074790fb7a2ecfab29b4c4c623b
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-27 01:17:55 -0300

    add: Export GameMesh

--- src/classes/Game/index.ts
+++ src/classes/Game/index.ts
@@ -1,3 +1,4 @@
 export { default as Core } from "./Core.class";
+export { default as Mesh } from "./GameMesh.class";
 export { default as Scene } from "./GameScene.class";
 export { default as Stage } from "./GameStage.class";
