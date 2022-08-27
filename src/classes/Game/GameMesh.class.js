413a2c4f964e19c27a53316a73bcaa3009a22784
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-27 01:17:27 -0300

    add: Class to add proxies to geometry parameters

---
+++ src/classes/Game/GameMesh.class.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+
+class GameMesh extends THREE.Mesh {
+    constructor(geometry, material) {
+        super(geometry, material);
+
+        if (geometry) {
+            geometry.parameters = new Proxy(geometry.parameters, {
+                set: function(parameters, param, value) {
+                    const oldParameters = parameters;
+                    const newParameters = {
+                        ...oldParameters,
+                        [param]: value
+                    };
+
+                    const args = [];
+
+                    Object.entries(newParameters).forEach(([ key, value ]) => {
+                        args.push(value);
+                        parameters[key] = value;
+                    });
+
+                    const newGeometry = new THREE[geometry.type](...args);
+
+                    Object.keys(geometry).forEach((key) => {
+                        if ([ "uuid", "id", "parameters" ].includes(key)) {
+                            return;
+                        }
+                        
+                        geometry[key] = newGeometry[key];
+                    });
+
+                    return true;
+                }
+            });
+        }
+    }
+}
+
+export default GameMesh;
