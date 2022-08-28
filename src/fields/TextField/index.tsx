5efd27d84fba6e32ff455ffd6aa1140cad6b283c
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-27 21:42:50 -0300

    add: Field that only accepts texts

---
+++ src/fields/TextField/index.tsx
@@ -0,0 +1,75 @@
+import { useContext, useEffect, useState } from "react";
+import { 
+    TextField as MuiTextField, 
+    TextFieldProps as MuiTextFieldProps 
+} from "@mui/material";
+import { EditorContext } from "@local/contexts";
+import { getProperty, setProperty } from "@local/functions";
+import { FieldProps } from "../index";
+import { t } from "@local/i18n";
+import "@local/styles/fields/TextField.scss";
+
+function TextField(props: FieldProps & MuiTextFieldProps) {
+    const { 
+        attributes, 
+        labels,
+        minLength,
+        maxLength,
+        scope,
+        ...textFieldProps
+    } = props;
+    const attrPath = attributes[0];
+    const label = labels[0];
+    const editor = useContext(EditorContext);
+
+    const [value, setValue] = useState<string>("");
+
+    useEffect(() => {
+        if (!editor) {
+            return;
+        }
+
+        const object = getProperty<Object | undefined>(scope, editor.transformControls);
+
+        if (object) {
+            setValue(
+                getProperty<string>(attrPath, object)
+            );
+        }
+    }, [editor?.transformControls.object]);
+
+    useEffect(() => {
+        if (!editor) {
+            return;
+        }
+
+        const object = getProperty<Object | undefined>(scope, editor.transformControls);
+        const helper = editor.transformControls.helper || null;
+
+        if (object) {
+            setProperty(attrPath, value, object);
+            helper?.update();
+        }
+    }, [value]);
+
+    return (
+        <MuiTextField 
+            className="TextField"
+
+            label={t(label)}
+
+            onChange={evt => setValue(evt.target.value)}
+            value={value}
+
+            inputProps={{
+                type: "text",
+                maxLength,
+                minLength
+            }}
+
+            {...textFieldProps}
+        />
+    );
+}
+
+export default TextField;
--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,10 +1,12 @@
 export interface FieldProps {
-    scope: "object" | "object.geometry" | "object.material",
-    labels: string[],
-    attributes: string[],
-    step?: number,
-    min?: number,
-    max?: number
+    scope: "object" | "object.geometry" | "object.material";
+    labels: string[];
+    attributes: string[];
+    step?: number;
+    min?: number;
+    max?: number;
+    maxLength?: number;
+    minLength?: number;
 }
 
 export { default as CheckboxField } from "./CheckboxField";
@@ -12,3 +14,4 @@ export { default as ColorField } from "./ColorField";
 export { default as MultiNumberField } from "./MultiNumberField";
 export { default as OptionsField } from "./OptionsField";
 export { default as SingleNumberField } from "./SingleNumberField";
+export { default as TextField } from "./TextField";
