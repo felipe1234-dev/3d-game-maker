84195cc72eeb69c41b585165cf114f9ff92575f8
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-30 19:21:52 -0300

    add: Import new components

--- src/pages/Editor/Menubar/index.tsx
+++ src/pages/Editor/Menubar/index.tsx
@@ -1,23 +1,17 @@
-import React from "react";
-import { 
-    AppBar, 
-    Box, 
-    Toolbar, 
-    IconButton 
-} from "@mui/material";
-import { ChevronLeft as ArrowLeftIcon } from "@mui/icons-material";
+import { AppBar, Box, Toolbar } from "@mui/material";
 
-import FileItem from "./FileItem";
 import EditItem from "./EditItem";
-import AddItem from "./AddItem";
+import Project from "./Project";
+import LangSelector from "./LangSelector";
 
 function Menubar() {
     return (
         <AppBar className="Editor-menubar" component="header">
             <Toolbar>
-                <FileItem />
                 <EditItem />
-                <AddItem />
+                <Project />
+                <Box flexGrow={1} />
+                <LangSelector />
             </Toolbar>
         </AppBar>
     );
