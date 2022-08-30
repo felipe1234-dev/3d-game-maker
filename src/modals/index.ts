5868040d97bbac8e0a5c04381ad998923010d94c
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-30 19:23:22 -0300

    add: export new modals

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -1,7 +1,11 @@
+export { default as EditTextureModal } from "./Editor/Texture/Edit";
+
 export { default as EditGeometryModal } from "./Editor/Geometry/Edit";
 
 export { default as EditObjectModal } from "./Editor/Object/Edit";
 export { default as AddObjectModal } from "./Editor/Object/Add";
+
+export { default as EditProjectModal } from "./Editor/Project/Edit";
 
 export { default as EditSceneModal } from "./Editor/Scene/Edit";
 export { default as EditSceneTreeModal } from "./Editor/SceneTree/Edit";
