9e9283a0584a7e90ded4bfcf29068ebca9bb1424
Felipe Alves <felipe.alves@taugor.com.br>
2022-08-30 22:22:59 -0300

    fix: Type of color state is THREE.Color now

--- src/fields/ColorField/index.tsx
+++ src/fields/ColorField/index.tsx
@@ -5,10 +5,13 @@ import { FieldProps } from "../index";
 import { getProperty, setProperty } from "@local/functions";
 import { t } from "@local/i18n";
 import ColorPicker from "material-ui-color-picker";
+import * as THREE from "three";
 
 function ColorField(props: FieldProps) {
     const defaultColor = "#fff";
-    const [color, setColor] = useState<string>(defaultColor);
+    const [color, setColor] = useState<THREE.Color>(
+        new THREE.Color(defaultColor)
+    );
 
     const { labels, attributes, scope } = props;
     const label = t(labels[0]);
@@ -23,7 +26,9 @@ function ColorField(props: FieldProps) {
         const object = getProperty<object | undefined | null>(scope, editor.transformControls);
 
         if (object) {
-            setColor(getProperty<string>(attrPath, object));
+            setColor(
+                getProperty<THREE.Color>(attrPath, object)
+            );
         }
     }, [editor?.transformControls.object]);
 
@@ -36,14 +41,27 @@ function ColorField(props: FieldProps) {
 
         if (object) {
             setProperty(attrPath, color, object);
+
+            if (object instanceof THREE.Material) {
+                object.needsUpdate = true;
+            }
         }
     }, [color]);
 
     return (
         <ColorPicker 
             variant="outlined"
-            onChange={(value) => setColor(value)}
-            value={color}
+            label={label}
+
+            onChange={value => setColor(new THREE.Color(value))}
+            value={"#" + color.getHexString()}
+
+            InputProps={{
+                style: {
+                    color: "#" + color.getHexString()
+                },
+                value: "#" + color.getHexString()
+            }}
         />
     );
 }
