16d93307f005101e51a69cf50d2f012716463f08
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-04 15:45:31 -0300

    add: Modal to change game name and description

--- src/modals/Editor/Project/Edit/Body.tsx
+++ src/modals/Editor/Project/Edit/Body.tsx
@@ -1,6 +1,54 @@
+import { useContext, useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import { t } from "@local/i18n";
+import { GameContext } from "@local/contexts";
+
 function Body() {
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const game = useContext(GameContext);
+
+    useEffect(() => {
+        if (!game) {
+            return;
+        }
+
+        setName(game.name);
+        setDescription(game.description);
+    }, [game]);
+
+    useEffect(() => {
+        if (!game) {
+            return;
+        }
+        
+        game.name = name;
+        game.description = description;
+    }, [name, description]);
+
     return (
-        <></>
+        <>
+            <TextField
+                label={t("Name")}
+                variant="outlined"
+
+                onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                    setName(evt.target.value);
+                }}
+                value={name}
+
+                sx={{ mb: 2 }}
+            />
+            <TextField
+                label={t("Description")}
+                variant="outlined"
+
+                onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                    setDescription(evt.target.value);
+                }}
+                value={description}
+            />
+        </>
     );
 }
 
