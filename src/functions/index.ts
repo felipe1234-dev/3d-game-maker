daad3acbc9037cae388fd253ed8d37dc381ea5e8
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-04 15:42:46 -0300

    add: Convert RGB to Hex

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,6 +2,7 @@ export { default as getEnv } from "./getEnv";
 export { default as getProperty } from "./getProperty";
 export { default as isLocation } from "./isLocation";
 export { default as isRouteState } from "./isRouteState";
+export { default as rgbToHex } from "./rgbToHex";
 export { default as setProperty } from "./setProperty";
 export { default as stringToAvatar } from "./stringToAvatar";
 export { default as stringToColor } from "./stringToColor";
---
+++ src/functions/rgbToHex.ts
@@ -0,0 +1,9 @@
+export default function rgbToHex(r: number, g: number, b: number): string {
+    if (r > 255 || g > 255 || b > 255) {
+        throw "Invalid color component";
+    }
+
+    const hex = ((r << 16) | (g << 8) | b).toString(16);
+
+    return "#" + ("000000" + hex).slice(-6);
+};
