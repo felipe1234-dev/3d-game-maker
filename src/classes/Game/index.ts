6b119e8da8f34c3390fb30165093f250114c3093
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-07 01:22:59 -0300

    add: Export Game.Physics class

--- src/classes/Game/index.ts
+++ src/classes/Game/index.ts
@@ -2,3 +2,4 @@ export { default as Core } from "./GameCore.class";
 export { default as Mesh } from "./GameMesh.class";
 export { default as Scene } from "./GameScene.class";
 export { default as Stage } from "./GameStage.class";
+export { default as Physics } from "./GamePhysics.class";
