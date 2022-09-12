04232e19c3798237d4d60ac81d64894f16d98e80
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-12 00:07:54 -0300

    add: Helpers

--- src/classes/Editor/index.ts
+++ src/classes/Editor/index.ts
@@ -2,3 +2,5 @@ export { default as Core } from "./EditorCore.class";
 export { default as Renderer } from "./EditorRenderer.class";
 export { default as Orbit } from "./EditorOrbit.class";
 export { default as Transform } from "./EditorTransform.class";
+
+export * as Helpers from "./Helpers";
