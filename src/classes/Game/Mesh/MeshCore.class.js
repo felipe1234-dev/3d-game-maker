eef20889a8ebc30d8a4388bf7280422fd7ecbac7
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-15 00:05:14 -0300

    add: Physical body with Cannon-es

--- src/classes/Game/Mesh/MeshCore.class.js
+++ src/classes/Game/Mesh/MeshCore.class.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import * as CANNON from "cannon-es";
 import { threeToCannon } from "three-to-cannon";
+import MeshBody from "./MeshBody.class";
 
 class MeshCore extends THREE.Mesh {
     /**
-     * @param {THREE.BufferGeometry=} geometry 
-     * @param {THREE.Material=} material 
+     * @param {THREE.BufferGeometry=} geometry
+     * @param {THREE.Material=} material
      * @param {{
      *     hitboxSize?: number;
      * }=} physics
@@ -13,18 +14,20 @@ class MeshCore extends THREE.Mesh {
     constructor(geometry, material, physics = {}) {
         super(geometry, material);
 
+        const scope = this;
+
         if (geometry) {
             geometry.parameters = new Proxy(geometry.parameters, {
-                set: function(parameters, param, value) {
+                set: function (parameters, param, value) {
                     const oldParameters = parameters;
                     const newParameters = {
                         ...oldParameters,
-                        [param]: value
+                        [param]: value,
                     };
 
                     const args = [];
 
-                    Object.entries(newParameters).forEach(([ key, value ]) => {
+                    Object.entries(newParameters).forEach(([key, value]) => {
                         args.push(value);
                         parameters[key] = value;
                     });
@@ -32,51 +35,106 @@ class MeshCore extends THREE.Mesh {
                     const newGeometry = new THREE[geometry.type](...args);
 
                     Object.keys(geometry).forEach((key) => {
-                        if ([ "uuid", "id", "parameters" ].includes(key)) {
+                        if (["uuid", "id", "parameters"].includes(key)) {
                             return;
                         }
-                        
+
                         geometry[key] = newGeometry[key];
                     });
 
+                    const mesh = new THREE.Mesh(geometry);
+                    const { shape } = threeToCannon(mesh) || {};
+                    scope.body = new MeshBody({
+                        mesh: scope,
+                        shape,
+                        collisionFilterGroup: scope.body.collisionFilterGroup,
+                        collisionFilterMask: scope.body.collisionFilterMask,
+                        collisionResponse: scope.body.collisionResponse,
+                        position: scope.body.position,
+                        velocity: scope.body.velocity,
+                        mass: scope.body.mass,
+                        material: scope.body.material,
+                        linearDamping: scope.body.linearDamping,
+                        type: scope.body.type,
+                        allowSleep: scope.body.allowSleep,
+                        sleepSpeedLimit: scope.body.sleepSpeedLimit,
+                        sleepTimeLimit: scope.body.sleepTimeLimit,
+                        quaternion: scope.body.quaternion,
+                        angularVelocity: scope.body.angularVelocity,
+                        fixedRotation: scope.body.fixedRotation,
+                        angularDamping: scope.body.angularDamping,
+                        linearFactor: scope.body.linearFactor,
+                        angularFactor: scope.body.angularFactor,
+                        isTrigger: scope.body.isTrigger,
+                    });
+
                     return true;
-                }
+                },
             });
         }
 
-        const {
-            hitboxSize = 1.3
-        } = physics;
-        
+        const { hitboxSize = 1.3 } = physics;
+
         const result = threeToCannon(this) || {};
         const { shape } = result;
-        
-        const { x, y, z } = this.position;
+
+        const { x: px, y: py, z: pz } = this.position;
+        const { x: qx, y: qy, z: qz, w } = this.quaternion;
+
         /**
-         * @type {CANNON.Body}
+         * @type {MeshBody}
          * @public
          */
-        this.body = new CANNON.Body({
-            position: new CANNON.Vec3(x, y, z),
-            shape
+        this.body = new MeshBody({
+            mesh: this,
+            position: new CANNON.Vec3(px, py, pz),
+            quaternion: new CANNON.Quaternion(qx, qy, qz, w),
+            shape,
         });
-        
-        /* const scope = this;
+
         this.position = new Proxy(this.position, {
-            set: function(position, axis, value) {
+            set: function (position, axis, value) {
                 position[axis] = Number(value);
-                
-                scope.body.position.copy(position);
-                scope.body.quaternion.copy(position);
+
+                const { x, y, z } = position;
+                scope.body.position.copy(new CANNON.Vec3(x, y, z));
 
                 return true;
-            }
-        }); */
+            },
+        });
 
-        console.log(this.body);
+        this.rotation = new Proxy(this.rotation, {
+            set: function (rotation, axis, value) {
+                rotation[axis] = Number(value);
+                rotation._onChangeCallback();
+
+                const { x, y, z, order } = rotation;
+                scope.body.quaternion.setFromEuler(x, y, z, order);
+
+                return true;
+            },
+        });
+
+        this.scale = new Proxy(this.scale, {
+            set: function (scale, axis, value) {
+                scale[axis] = Number(value);
+
+                return true;
+            },
+        });
+
+        this.receiveShadow = true;
+        this.castShadow = true;
     }
 
-
+    /**
+     * @param {boolean} bool
+     */
+    set needsUpdate(bool) {
+        if (!this.body || !bool) {
+            return;
+        }
+    }
 }
 
 export default MeshCore;
