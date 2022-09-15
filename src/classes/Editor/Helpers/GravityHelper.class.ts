608015685ad4efd84aa695643a4b2343e2885a30
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-15 00:06:20 -0300

    add: Gravity plane always facing center

--- src/classes/Editor/Helpers/GravityHelper.class.ts
+++ src/classes/Editor/Helpers/GravityHelper.class.ts
@@ -1,16 +1,26 @@
 import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import { Editor } from "@local/classes";
 import SphericalGrid from "./SphericalGrid.class";
 
 class GravityHelper extends THREE.Mesh {
+    public editor: Editor.Core;
     public plane: SphericalGrid;
-    //public pointMass: THREE.Mesh<THREE.SphereGeometry>;
+    public massPoint: THREE.Mesh<THREE.SphereGeometry>;
+    public gx: number;
+    public gy: number;
+    public gz: number;
+    public orbitRadius: number;
 
-    constructor() {
+    constructor(editor: Editor.Core, orbitRadius?: number) {
         super();
 
+        this.editor = editor;
+        this.orbitRadius = orbitRadius ?? Infinity;
+
         const color = "#62dafb";
-        const radius = 8;
-        const offset = 3;
+        const radius = 5;
+        const offset = 1;
         const nLines = 8;
         const nCircles = 3;
 
@@ -21,34 +31,41 @@ class GravityHelper extends THREE.Mesh {
             nLines,
             nCircles
         );
+        this.plane.rotation.x = -Math.PI/2;
 
         this.add(this.plane);
+        
+        this.gx = 0;
+        this.gy = 0;
+        this.gz = 0;
 
-        /* const sphereGeometry = new THREE.SphereGeometry(0.5, 10, 10);
-        const wireframe = new THREE.WireframeGeometry(sphereGeometry);
-        const sphereMaterial = new THREE.MeshBasicMaterial({ 
-            color,
-            wireframe: true,
-            opacity: 0.25,
-            transparent: true
-        });
-        const sphere = new THREE.LineSegments(wireframe, sphereMaterial);
+        const geometry = new THREE.SphereGeometry(0.5, 64, 64);
+        const material = new THREE.MeshBasicMaterial({ color });
+        this.massPoint = new THREE.Mesh(geometry, material);
+        this.massPoint.position.set(0, 0, 0);
 
-        sphere.position.set(0, 0, 0);
-
-        this.add(sphere); */
+        this.add(this.massPoint);
     }
 
-    set gx(value: number) {
-        this.position.x = value;
-    }
+    set needsUpdate(bool: boolean) {
+        if (!bool) return;
 
-    set gy(value: number) {
-        this.position.y = value;
-    }
+        const gVector = new THREE.Vector3(
+            this.gx,
+            this.gy,
+            this.gz
+        );
 
-    set gz(value: number) {
-        this.position.z = value;
+        const axes = ["x", "y", "z"] as const;
+        for (const ax of axes) {
+            const value = gVector[ax];
+
+            this.position[ax] = Math.min(Math.abs(value), this.orbitRadius);
+            this.position[ax] *= value < 0 ? -1 : +1;
+        }
+
+        const center = new THREE.Vector3(0, 0, 0);
+        this.lookAt(center);
     }
 }
 
