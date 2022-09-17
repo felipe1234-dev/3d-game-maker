b9779a36cd7dbf900f84f80ab884f415673a2dbd
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-17 15:17:48 -0300

    add: Support for a modal with no backdrop

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,42 +1,38 @@
-import React from "react";
-import { 
+import React, {
+    forwardRef,
+    ReactElement,
+    ReactNode,
+    Ref,
+    useContext,
+    useState,
+} from "react";
+import {
     Slide,
-
-    Dialog, 
+    Dialog,
     DialogProps,
-    
     Paper,
     PaperProps,
-
     DialogTitle,
     DialogTitleProps,
-
     DialogContent,
-    DialogActions
+    DialogActions,
+    IconButton,
 } from "@mui/material";
 import { TransitionProps } from "@mui/material/transitions";
-import { useNavigate } from "react-router-dom";
 import { Resizable } from "react-resizable";
-import { getLang } from "@local/i18n";
+import { CloseOutline } from "@styled-icons/evaicons-outline";
 import Draggable from "react-draggable";
+
+import { HistoryContext } from "@local/contexts";
+
 import "@local/styles/components/Modal.scss";
 
-const Transition = (
-    React.forwardRef(
-        function Transition(
-            props: TransitionProps & { children: React.ReactElement<any, any> },
-            ref: React.Ref<unknown>
-        ) {
-            return (
-                <Slide 
-                    ref={ref}
-                    direction="up"  
-                    {...props} 
-                />
-            );
-        }
-    )
-);
+const Transition = forwardRef(function Transition(
+    props: TransitionProps & { children: ReactElement<any, any> },
+    ref: Ref<unknown>
+) {
+    return <Slide ref={ref} direction="up" {...props} />;
+});
 
 const DraggablePaper = (props: PaperProps) => (
     <Draggable
@@ -48,18 +44,26 @@ const DraggablePaper = (props: PaperProps) => (
 );
 
 interface ModalProps extends Omit<DialogProps, "open"> {
-    className?: string,
-    placement?: 
-        "center" | "top-center" | "bottom-center" | "top-right" | 
-        "top-left" | "bottom-left" | "bottom-right",
-    onClose?: () => void,
-    resizable?: boolean,
-    draggable?: boolean,
-    height?: number,
-    width?: number, 
-    header?: React.ReactNode,
-    body?: React.ReactNode,
-    footer?: React.ReactNode
+    className?: string;
+    placement?:
+        | "top"
+        | "top-left"
+        | "top-right"
+        | "center"
+        | "center-left"
+        | "center-right"
+        | "bottom"
+        | "bottom-left"
+        | "bottom-right";
+    onClose?: () => void;
+    resizable?: boolean;
+    draggable?: boolean;
+    height?: number;
+    width?: number;
+    header?: ReactNode;
+    body?: ReactNode;
+    footer?: ReactNode;
+    disableBackdrop?: boolean;
 }
 
 function Modal(props: ModalProps) {
@@ -74,19 +78,19 @@ function Modal(props: ModalProps) {
         header,
         body,
         footer,
-        ...dialogProps 
+        disableBackdrop,
+        ...dialogProps
     } = props;
 
     customClassName = customClassName ?? "";
     placement = placement ?? "center";
 
     let className = "Modal " + customClassName;
-    className += " Modal--" + ("is-" + placement)
-        .toLowerCase()
-        .replace(
-            /[^a-zA-Z0-9]+(.)/g, 
-            (m, chr) => chr.toUpperCase()
-        );
+    className +=
+        " Modal--" +
+        ("is-" + placement)
+            .toLowerCase()
+            .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
     if (draggable) {
         className += " Modal--isDraggable";
     }
@@ -95,44 +99,60 @@ function Modal(props: ModalProps) {
         className += " Modal--isResizable";
     }
 
+    if (disableBackdrop) {
+        className += " Modal--backdropDisabled";
+    }
+
     let headerProps: DialogTitleProps = {};
     if (draggable) {
         headerProps.className = "Modal--isDraggable-handle";
     }
 
-    const [open, setOpen] = React.useState<boolean>(true);
-    const [height, setHeight] = React.useState<number>(initialHeight ?? 100);
-    const [width, setWidth] = React.useState<number>(initialWidth ?? 300);
-    const lang = getLang();
-    const navigate = useNavigate();
-    
-    const goBack = () => {
-        navigate(`/${lang}/editor/`, {
-            state: { 
-                useLoader: false 
+    const history = useContext(HistoryContext);
+
+    const [open, setOpen] = useState(true);
+    const [height, setHeight] = useState(initialHeight ?? 100);
+    const [width, setWidth] = useState(initialWidth ?? 300);
+
+    const closeModal = (reason: string) => {
+        if (disableBackdrop && reason === "backdropClick") {
+            return;
+        }
+
+        setOpen(false);
+
+        setTimeout(() => {
+            if (onClose) {
+                onClose();
+            } else {
+                history.back({
+                    state: {
+                        useLoader: false,
+                    },
+                });
             }
-        });
-    }
+        }, 1000);
+    };
 
     const Content = () => (
         <>
             {header && (
                 <DialogTitle {...headerProps}>
                     {header}
+                    {disableBackdrop && (
+                        <IconButton
+                            className="Modal--backdropDisabled-closeButton"
+                            onClick={() => closeModal("closeButtonClick")}
+                        >
+                            <CloseOutline width={24} />
+                        </IconButton>
+                    )}
                 </DialogTitle>
             )}
-            {body && (
-                <DialogContent>
-                    {body}
-                </DialogContent>
-            )}
-            {footer && (
-                <DialogActions>
-                    {footer}
-                </DialogActions>
-            )}
+            {body && <DialogContent>{body}</DialogContent>}
+            {footer && <DialogActions>{footer}</DialogActions>}
         </>
-    )
+    );
 
     return (
         <Dialog
@@ -140,19 +160,13 @@ function Modal(props: ModalProps) {
             keepMounted
             open={open}
             TransitionComponent={Transition}
-            onClose={() => {
-                setOpen(false);
-
-                setTimeout(() => {
-                    if (onClose) {
-                        onClose();
-                    } else {
-                        goBack();
-                    }
-                }, 1000);
-            }}
+            onClose={(evt, reason) => closeModal(reason)}
             PaperComponent={draggable ? DraggablePaper : Paper}
             PaperProps={{ style: { width, height } }}
+            style={{
+                ["--width" as any]: `${width}px`,
+                ["--height" as any]: `${height}px`,
+            }}
             {...dialogProps}
         >
             {resizable ? (
@@ -163,15 +177,15 @@ function Modal(props: ModalProps) {
                         setHeight(data.size.height);
                         setWidth(data.size.width);
                     }}
-                    handle={
-                        <span className="Modal--isResizable-handle" />
-                    }
+                    handle={<span className="Modal--isResizable-handle" />}
                 >
-                    <div className="Modal--isResizable-container"> 
+                    <div className="Modal--isResizable-container">
                         <Content />
                     </div>
                 </Resizable>
-            ) : <Content />}
+            ) : (
+                <Content />
+            )}
         </Dialog>
     );
 }
