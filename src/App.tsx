d21e7e1aa881a69c851c99a98c975914cba1f299
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-17 15:22:09 -0300

    style: Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 import { getLang, t } from "./i18n";
 import { Composer } from "./components";
-import { AlertProvider } from "./contexts";
+import { AlertProvider, HistoryProvider } from "./contexts";
 
 import routes from "./consts/routes";
 import Root from "./Root";
@@ -11,20 +11,22 @@ import Root from "./Root";
 function App() {
     const { pathname } = useLocation();
 
-    const providers = useMemo(() => (
-        routes.pages.filter(item => (
-            pathname.includes(item.path)
-        )).reduce((resu: Function[], item) => {
-            resu.push(...item.providers);
-            return resu;
-        }, [])
-    ), [pathname]);
+    const providers = useMemo(
+        () =>
+            routes.pages
+                .filter(item => pathname.includes(item.path))
+                .reduce((resu: Function[], item) => {
+                    resu.push(...item.providers);
+                    return resu;
+                }, []),
+        [pathname]
+    );
 
-    const pageTitle = useMemo(() => (
-        routes.pages.find(item => (
-            pathname.includes(item.path)
-        ))?.pageTitle
-    ), [pathname]);
+    const pageTitle = useMemo(
+        () =>
+            routes.pages.find(item => pathname.includes(item.path))?.pageTitle,
+        [pathname]
+    );
 
     useEffect(() => {
         const lang = getLang();
@@ -42,14 +44,14 @@ function App() {
 
         document.title = t(pageTitle);
 
-        sessionStorage.setItem("Last Activity", (new Date().getTime()).toString());
+        sessionStorage.setItem(
+            "Last Activity",
+            new Date().getTime().toString()
+        );
     }, [pathname]);
 
     return (
-        <Composer components={[
-            AlertProvider,
-            ...providers
-        ]}>
+        <Composer components={[AlertProvider, HistoryProvider, ...providers]}>
             <Root />
         </Composer>
     );
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect, useState } from "react";
+import { Box } from "@mui/material";
 import {
     Routes as Switch,
     Route,
     useLocation,
-    Location
+    Location,
 } from "react-router-dom";
-import { Box } from "@mui/material";
-import { 
-    AlertMessage, 
-    PageLoader,
-    Composer
-} from "@local/components";
+import { AlertMessage, PageLoader, Composer } from "@local/components";
 import { AlertContext } from "@local/contexts";
 import { isRouteState } from "@local/functions";
 
@@ -18,34 +14,30 @@ import routes from "@local/consts/routes";
 
 function Root() {
     const [pageIsLoading, setPageIsLoading] = useState<boolean>(true);
-    const {
-        message,
-        setMessage,
-        severity,
-        setSeverity
-    } = useContext(AlertContext);
-    
+    const { message, setMessage, severity, setSeverity } =
+        useContext(AlertContext);
+
     const pageLocation = useLocation();
     const { pathname: pathNow, state } = pageLocation;
-    
+
     let backgroundLocation: Location | null = null;
     let enablePageLoader: boolean = true;
-    
+
     if (isRouteState(state)) {
         backgroundLocation = state.background ?? null;
         enablePageLoader = state.useLoader ?? false;
     }
-    
+
     useEffect(() => {
         setPageIsLoading(enablePageLoader);
     }, [pathNow]);
-    
+
     useEffect(() => {
         setTimeout(() => {
             setPageIsLoading(false);
         }, 3000);
     });
-    
+
     return (
         <>
             <Box component="main">
@@ -54,33 +46,37 @@ function Root() {
                         <Route
                             key={i}
                             path={`/:lang${item.path}`}
-                            element={(
-                                <Composer components={[
-                                    ...item.wrappers,
-                                    item.Element
-                                ]} />
-                            )}
+                            element={
+                                <Composer
+                                    components={[
+                                        ...item.wrappers,
+                                        item.Element,
+                                    ]}
+                                />
+                            }
                         />
                     ))}
                 </Switch>
-                
+
                 {backgroundLocation && (
                     <Switch>
                         {routes.modals.map((item, i) => (
                             <Route
                                 key={i}
                                 path={`/:lang${item.path}`}
-                                element={(
-                                    <Composer components={[
-                                        ...item.wrappers,
-                                        item.Element
-                                    ]} />
-                                )}
-                            />  
+                                element={
+                                    <Composer
+                                        components={[
+                                            ...item.wrappers,
+                                            item.Element,
+                                        ]}
+                                    />
+                                }
+                            />
                         ))}
                     </Switch>
                 )}
-                
+
                 <AlertMessage
                     open={!!message && !!severity}
                     type={severity ?? undefined}
