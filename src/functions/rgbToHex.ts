1799cccde3cd6ef998ae611b7c84ba5a062d54d0
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-18 13:41:22 -0300

    fix: es-lint fix

--- src/functions/rgbToHex.ts
+++ src/functions/rgbToHex.ts
@@ -1,6 +1,6 @@
 export default function rgbToHex(r: number, g: number, b: number): string {
     if (r > 255 || g > 255 || b > 255) {
-        throw "Invalid color component";
+        throw new Error("Invalid color component");
     }
 
     const hex = ((r << 16) | (g << 8) | b).toString(16);
