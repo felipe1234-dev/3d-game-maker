b99fe6104dc87b93bc357ac40510e8e334693084
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-23 23:01:28 -0300

    add: Exporting formats

--- src/classes/Game/index.ts
+++ src/classes/Game/index.ts
@@ -1,4 +1,6 @@
+// Base
 export { default as Core } from "./GameCore.class";
+export { default as Renderer } from "./GameRenderer.class";
 export { default as Mesh } from "./Mesh/MeshCore.class";
 export { default as Stage } from "./Stage/StageCore.class";
 export { default as Scene } from "./Scene/SceneCore.class";
@@ -15,6 +17,12 @@ export { default as DirectionalLight } from "./Lights/DirectionalLight.class";
 export { default as HemisphereLight } from "./Lights/HemisphereLight.class";
 export { default as PointLight } from "./Lights/PointLight.class";
 export { default as SpotLight } from "./Lights/SpotLight.class";
+
+// Formats 
+export type { default as GameFormat } from "./Formats/GameFormat.interface";
+export type { default as StageFormat } from "./Formats/StageFormat.interface";
+export type { default as SceneFormat } from "./Formats/SceneFormat.interface";
+export type { default as RendererFormat } from "./Formats/RendererFormat.interface";
 
 // Interfaces
 export type { default as Camera } from "./Interfaces/GameCamera.interface";
