3fb4b12bee0373553ffc3154da51781dffa01684
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-25 12:31:15 -0300

    add: Base object format

---
+++ src/classes/Game/Formats/ObjectFormat.interface.ts
@@ -0,0 +1,15 @@
+interface ObjectFormat {
+    uuid: string;
+    type: string;
+    name: string;
+    matrix: number[];
+    children?: ObjectFormat[];
+    receiveShadow?: boolean;
+    castShadow?: boolean;
+    visible?: boolean;
+    frustumCulled?: boolean;
+    renderOrder?: number;
+    userData?: any;
+}
+
+export default ObjectFormat;
