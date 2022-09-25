178575b44085ac23b517ab74bc5bb6d66aeeadaf
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-25 13:15:33 -0300

    fix: Use ObjectFormat and PhysicsFormat

--- src/classes/Game/Formats/SceneFormat.interface.ts
+++ src/classes/Game/Formats/SceneFormat.interface.ts
@@ -1,5 +1,6 @@
 import PhysicsFormat from "./PhysicsFormat.interface";
 import BodyFormat from "./BodyFormat.interface";
+import ObjectFormat from "./ObjectFormat.interface";
 
 interface SceneFormat {
     metadata: {
@@ -10,14 +11,15 @@ interface SceneFormat {
     geometries: object[];
     materials: object[];
     bodies: BodyFormat[];
+    physics: PhysicsFormat;
     object: {
         uuid: string;
+        name: string;
         game?: string;
         stage?: string;
         type: "Scene";
         matrix: number[];
-        children: object[];
-        physics: PhysicsFormat;
+        children: ObjectFormat[];
     };
 }
 
