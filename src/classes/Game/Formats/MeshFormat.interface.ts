ab6b4d3683bf5b4afb436da11a822b789817ab96
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-25 12:33:33 -0300

    add: Mesh format

---
+++ src/classes/Game/Formats/MeshFormat.interface.ts
@@ -0,0 +1,10 @@
+import ObjectFormat from "./ObjectFormat.interface";
+
+interface MeshFormat extends ObjectFormat {
+    type: "Mesh";
+    geometry?: string;
+    material?: string;
+    body?: string;
+}
+
+export default MeshFormat;
