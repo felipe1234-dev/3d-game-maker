9552e2686a8c85364f723b5a547ed1c086087fbe
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-26 13:43:24 -0300

    fix: Changes in directories

--- src/classes/Game/index.ts
+++ src/classes/Game/index.ts
@@ -2,8 +2,10 @@
 export { default as Core } from "./GameCore.class";
 export { default as Renderer } from "./GameRenderer.class";
 export { default as Mesh } from "./Mesh/MeshCore.class";
+export { default as Body } from "./Mesh/MeshBody.class";
 export { default as Stage } from "./Stage/StageCore.class";
 export { default as Scene } from "./Scene/SceneCore.class";
+export { default as Physics } from "./Scene/ScenePhysics.class";
 
 // Cameras
 export { default as PerspectiveCamera } from "./Cameras/PerspectiveCamera.class";
@@ -19,21 +21,30 @@ export { default as PointLight } from "./Lights/PointLight.class";
 export { default as SpotLight } from "./Lights/SpotLight.class";
 
 // Formats
+// -> Base
 export type { default as GameFormat } from "./Formats/GameFormat.interface";
 export type { default as StageFormat } from "./Formats/StageFormat.interface";
-export type { default as SceneFormat } from "./Formats/SceneFormat.interface";
 export type { default as RendererFormat } from "./Formats/RendererFormat.interface";
 export type { default as ObjectFormat } from "./Formats/ObjectFormat.interface";
-export type { default as MeshFormat } from "./Formats/MeshFormat.interface";
-export type { default as BodyFormat } from "./Formats/BodyFormat.interface";
-export type { default as PhysicsFormat } from "./Formats/PhysicsFormat.interface";
-export type { default as AmbientLightFormat } from "./Formats/AmbientLightFormat.interface";
-export type { default as DirectionalLightFormat } from "./Formats/DirectionalLightFormat.interface";
-export type { default as HemisphereLightFormat } from "./Formats/HemisphereLightFormat.interface";
-export type { default as PointLightFormat } from "./Formats/PointLightFormat.interface";
-export type { default as SpotLightFormat } from "./Formats/SpotLightFormat.interface";
+export type { default as MetaFormat } from "./Formats/MetaFormat.interface";
+// -> Mesh
+export type { default as MeshFormat } from "./Formats/Mesh/MeshFormat.interface";
+export type { default as BodyFormat } from "./Formats/Mesh/BodyFormat.interface";
+// -> Scene
+export type { default as SceneFormat } from "./Formats/Scene/SceneFormat.interface";
+export type { default as PhysicsFormat } from "./Formats/Scene/PhysicsFormat.interface";
+// -> Lights
+export type { default as AmbientLightFormat } from "./Formats/Lights/AmbientLightFormat.interface";
+export type { default as DirectionalLightFormat } from "./Formats/Lights/DirectionalLightFormat.interface";
+export type { default as HemisphereLightFormat } from "./Formats/Lights/HemisphereLightFormat.interface";
+export type { default as PointLightFormat } from "./Formats/Lights/PointLightFormat.interface";
+export type { default as SpotLightFormat } from "./Formats/Lights/SpotLightFormat.interface";
+
+export type { default as GeometryFormat } from "./Formats/Geometries/GeometryFormat.interface";
+export type { default as BoxGeometryFormat } from "./Formats/Geometries/BoxGeometryFormat.interface";
 
 // Interfaces
 export type { default as Camera } from "./Interfaces/GameCamera.interface";
 export type { default as Object } from "./Interfaces/GameObject.interface";
 export type { default as ObjectHelper } from "./Interfaces/ObjectHelper.interface";
+export type { default as BodyOptions } from "./Interfaces/BodyOptions.interface";
