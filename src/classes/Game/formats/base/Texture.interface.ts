de160a8a191867ab61a8003ad5c1397610ed39eb
Felipe Alves <felipe.alves@taugor.com.br>
2022-09-28 09:01:26 -0300

    add: Texture format

---
+++ src/classes/Game/formats/base/Texture.interface.ts
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+
+interface Texture {
+    uuid: string;
+    name: string;
+
+    type: THREE.TextureDataType;
+
+    image: string; // Image uuid
+
+    mapping: THREE.Mapping;
+
+    repeat: [x: number, y: number];
+    offset: [x: number, y: number];
+    center: [x: number, y: number];
+    wrap: [wrapS: THREE.Wrapping, wrapT: THREE.Wrapping];
+
+    rotation: number;
+    format: THREE.PixelFormat;
+    encoding: THREE.TextureEncoding;
+
+    minFilter: THREE.TextureFilter;
+    magFilter: THREE.TextureFilter;
+
+    anisotropy: number;
+
+    flipY: boolean;
+
+    premultiplyAlpha: boolean;
+    unpackAlignment: number;
+
+    userData: any;
+}
+
+export default Texture;
