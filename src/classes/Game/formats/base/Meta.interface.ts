ebe0378df9fc39fd6720f5c9dcdf743ed8137204
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-01 17:15:22 -0300

    fix: Fixing imports

--- src/classes/Game/formats/base/Meta.interface.ts
+++ src/classes/Game/formats/base/Meta.interface.ts
@@ -1,10 +1,8 @@
-import {
-    BodyFormat,
-    TextureFormat,
-    SourceFormat,
-    GeometryFormat,
-    MaterialFormat,
-} from ".";
+import BodyFormat from "./Body.interface";
+import TextureFormat from "./Texture.interface";
+import SourceFormat from "./Source.interface";
+import GeometryFormat from "./Geometry.interface";
+import MaterialFormat from "./Material.interface";
 
 interface Meta {
     geometries?: {
