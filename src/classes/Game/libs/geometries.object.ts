934e3e37cd740d406b1f82d05deecd0efaf3adef
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-01 16:38:05 -0300

    add: Array with all valid geometries

---
+++ src/classes/Game/libs/geometries.object.ts
@@ -0,0 +1,22 @@
+const geometries = [
+    "BoxGeometry",
+    "CapsuleGeometry",
+    "CircleGeometry",
+    "ConeGeometry",
+    "CylinderGeometry",
+    "DodecahedronGeometry",
+    "EdgesGeometry",
+    "IcosahedronGeometry",
+    "LatheGeometry",
+    "OctahedronGeometry",
+    "PlaneGeometry",
+    "PolyhedronGeometry",
+    "RingGeometry",
+    "SphereGeometry",
+    "TetrahedronGeometry",
+    "TorusGeometry",
+    "TorusKnotGeometry",
+    "WireframeGeometry",
+] as const;
+
+export default geometries;
