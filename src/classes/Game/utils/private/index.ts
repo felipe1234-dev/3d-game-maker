4ab5f482200b8127a54d9f004863dbd050c69ca7
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:36:03 -0300

    add(classes/Game/utils/private): export new functions

--- src/classes/Game/utils/private/index.ts
+++ src/classes/Game/utils/private/index.ts
@@ -1,5 +1,7 @@
 export { default as generateID } from "./generateID.function";
 export { default as getTypedArray } from "./getTypedArray.function";
 export { default as applyData } from "./applyData.function";
+export { default as applyObject3DJSON } from "./applyObject3DJSON.function";
 export { default as bufferAttributeFromJSON } from "./bufferAttributeFromJSON.function";
 export { default as metaFromSceneJSON } from "./metaFromSceneJSON.function";
+export { default as parseObjectChildren } from "./parseObjectChildren.function";
