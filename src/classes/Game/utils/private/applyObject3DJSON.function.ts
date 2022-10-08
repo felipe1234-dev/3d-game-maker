9adf7a75e44f91fccfdea02b4ed27d52a7a8151a
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:32:58 -0300

    add(classes/Game/utils/private): create function to apply JSON properties (that are universally present) to passed object

---
+++ src/classes/Game/utils/private/applyObject3DJSON.function.ts
@@ -0,0 +1,21 @@
+import { Game } from "@local/classes";
+import * as THREE from "three";
+
+/**
+ * Standard object conversion from JSON.
+ * Receives a Game.Formats.Object3D and applies the properties containing 
+ * in the JSON that mandatorily exist in all objects.
+ */
+function applyObject3DJSON(object: Game.Object3D, json: Game.Formats.Object3D): void {
+    const matrix = new THREE.Matrix4().fromArray(json.object.matrix);
+    object.applyMatrix4(matrix);
+
+    if (json.object.receiveShadow !== undefined) object.receiveShadow = json.object.receiveShadow;
+    if (json.object.castShadow !== undefined) object.castShadow = json.object.castShadow;
+    if (json.object.visible !== undefined) object.visible = json.object.visible;
+    if (json.object.frustumCulled !== undefined) object.frustumCulled = json.object.frustumCulled;
+    if (json.object.renderOrder !== undefined) object.renderOrder = json.object.renderOrder;
+    if (json.object.userData instanceof Object) object.userData = json.object.userData;
+}
+
+export default applyObject3DJSON;
