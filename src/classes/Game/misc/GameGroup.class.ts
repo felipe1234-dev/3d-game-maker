1c35e00dbd9141f30a6dd2dec8bd57b90a2ef1ec
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:47:22 -0300

    add(classes/Game/misc): add from/toJSONs to misc objects

--- src/classes/Game/misc/GameGroup.class.ts
+++ src/classes/Game/misc/GameGroup.class.ts
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import { Game } from "@local/classes";
+import { applyObject3DJSON, parseObjectChildren } from "../utils/private";
 
-class GameGroup extends THREE.Group implements Game.Object {
+class GameGroup extends THREE.Group implements Game.Object3D {
     public helper: THREE.BoxHelper;
 
     constructor() {
@@ -9,6 +10,37 @@ class GameGroup extends THREE.Group implements Game.Object {
 
         this.helper = new THREE.BoxHelper(this);
         this.helper.visible = false;
+    }
+
+    public override toJSON(meta?: Game.Formats.Meta): Game.Formats.Group {
+        const json: Game.Formats.Group = super.toJSON(
+            meta
+                ? {
+                    geometries: meta.geometries || {},
+                    materials: meta.materials || {},
+                    textures: meta.textures || {},
+                    images: meta.images || {},
+                }
+                : undefined
+        );
+
+        if (this.children.length === 0) {
+            json.object.children = [];
+        }
+
+        return json;
+    }
+
+    public static fromJSON(
+        json: Game.Formats.Object3D,
+        meta?: Game.Formats.Meta
+    ): GameGroup {
+        const group = new GameGroup();
+
+        applyObject3DJSON(group, json);
+        parseObjectChildren(group, json, meta);
+
+        return group;
     }
 }
 
