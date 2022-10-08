0ae491cb5989d3f50278fc4e1f62e09695164165
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:45:08 -0300

    add(classes/Game/Formats): create format for Game.PerspectiveCamera

---
+++ src/classes/Game/formats/cameras/PerspectiveCamera.interface.ts
@@ -0,0 +1,63 @@
+import { Object3D, isObject3D } from "../base/Object3D.interface";
+
+interface PerspectiveCamera extends Object3D {
+    object: Object3D["object"] & {
+        type: "PerspectiveCamera";
+        fov: number;
+        zoom: number;
+        near: number;
+        far: number;
+        focus: number;
+        aspect: number;
+        view?: {
+            enabled: boolean;
+            fullWidth: number;
+            fullHeight: number;
+            offsetX: number;
+            offsetY: number;
+            width: number;
+            height: number;
+        },
+        filmGauge: number;
+        filmOffset: number;
+    };
+}
+
+function isPerspectiveCamera(json: any): json is PerspectiveCamera {
+    if (!(json instanceof Object)) return false;
+    if (!(json.object instanceof Object)) return false;
+
+    if (json.object.type !== "PerspectiveCamera") return false;
+
+    const numberKeys = [
+        "fov",
+        "zoom",
+        "near",
+        "far",
+        "focus",
+        "aspect",
+        "filmGauge",
+        "filmOffset",
+    ];
+
+    for (const key of numberKeys) {
+        if (typeof json.object[key] !== "number") return false;
+    }
+
+    if (json.object.view instanceof Object) {
+        if (typeof json.object.view.enabled !== "boolean") return false;
+        if (typeof json.object.view.fullWidth !== "number") return false;
+        if (typeof json.object.view.fullHeight !== "number") return false;
+        if (typeof json.object.view.offsetX !== "number") return false;
+        if (typeof json.object.view.offsetY !== "number") return false;
+        if (typeof json.object.view.width !== "number") return false;
+        if (typeof json.object.view.height !== "number") return false;
+    }
+
+    if (!isObject3D(json)) return false;
+
+    return true;
+}
+
+export type { PerspectiveCamera };
+export { isPerspectiveCamera };
--- src/classes/Game/formats/cameras/index.ts
+++ src/classes/Game/formats/cameras/index.ts
@@ -1,1 +1,1 @@
-export type {};
+export * from "./PerspectiveCamera.interface";
