6b84adaac71648e853e2b21cd14cfae29cf7831d
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:47:57 -0300

    del(classes/Game/models): delete unnecessary camera interface

--- src/classes/Game/models/GameCamera.interface.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-import { Game } from "@local/classes";
-
-interface GameCamera extends THREE.Camera {
-    game?: Game.Core;
-}
-
-export default GameCamera;
--- src/classes/Game/models/index.ts
+++ src/classes/Game/models/index.ts
@@ -1,3 +1,2 @@
-export type { default as Camera } from "./GameCamera.interface";
-export type { default as Object } from "./GameObject.interface";
+export type { default as Object3D } from "./GameObject3D.interface";
 export type { default as ObjectHelper } from "./ObjectHelper.interface";
