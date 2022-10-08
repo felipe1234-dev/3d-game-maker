34cb2676f0a0112a1858f4d95e84b2310abfa970
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:46:39 -0300

    add(classes/Game/cameras): add from/toJSONs to camera objects

--- src/classes/Game/cameras/PerspectiveCamera.class.ts
+++ src/classes/Game/cameras/PerspectiveCamera.class.ts
@@ -1,8 +1,44 @@
 import * as THREE from "three";
 import { Game } from "@local/classes";
+import { applyObject3DJSON, parseObjectChildren } from "../utils/private";
 
-class PerspectiveCamera extends THREE.PerspectiveCamera implements Game.Camera {
-    public game?: Game.Core;
+class PerspectiveCamera extends THREE.PerspectiveCamera implements Game.Object3D {
+    public override toJSON(
+        meta?: Game.Formats.Meta
+    ): Game.Formats.PerspectiveCamera {
+        return super.toJSON(meta);
+    }
+
+    public static fromJSON(
+        json: Game.Formats.PerspectiveCamera,
+        meta?: Game.Formats.Meta
+    ): PerspectiveCamera {
+        const camera = new PerspectiveCamera(
+            json.object.fov,
+            json.object.aspect,
+            json.object.near,
+            json.object.far
+        );
+
+        camera.id = json.object.id;
+        camera.uuid = json.object.uuid;
+        camera.name = json.object.name || "";
+
+        camera.zoom = json.object.zoom;
+        camera.focus = json.object.focus;
+
+        if (json.object.view) {
+            camera.view = json.object.view;
+        }
+
+        camera.filmGauge = json.object.filmGauge;
+        camera.filmOffset = json.object.filmOffset;
+
+        applyObject3DJSON(camera, json);
+        parseObjectChildren(camera, json, meta);
+
+        return camera;
+    }
 }
 
 export default PerspectiveCamera;
