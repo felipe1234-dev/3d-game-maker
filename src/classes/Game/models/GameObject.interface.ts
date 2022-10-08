5e4645c16e3235535d97f4b526c12d4f15fc58ca
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-08 20:39:36 -0300

    refactor(classes/Game/models): rename object model

--- src/classes/Game/models/GameObject.interface.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-import { Game } from "@local/classes";
-
-interface GameObject extends THREE.Object3D {
-    helper?: Game.ObjectHelper;
-    children: GameObject[] | THREE.Object3D[];
-}
-
-export default GameObject;
