e10830af008f88924dd2884e65b63d75bfa0f662
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-13 15:25:22 -0300

    add(Game/formats): add Material isMaterial function

--- src/classes/Game/formats/materials/Material.interface.ts
+++ src/classes/Game/formats/materials/Material.interface.ts
@@ -1,8 +1,9 @@
+import { materials } from "../../libs";
 import * as THREE from "three";
 
 interface Material {
     uuid: string;
-    type: string;
+    type: typeof materials[number];
     name?: string;
 
     alphaTest?: number;
@@ -42,43 +43,46 @@ function isMaterial(json: any): json is Material {
     if (!(json instanceof Object)) return false;
 
     if (typeof json.uuid !== "string") return false;
-    if (typeof json.name !== "string") return false;
-    if (json.name !== undefined) {
-        if (typeof json.type !== "string") return false;
-    }
+    if (
+        json.name !== undefined &&
+        typeof json.name !== "string"
+    ) return false;
+    if (![...materials].includes(json.type)) return false;
 
     const requiredNumbers = [
-        "alphaTest",
         "stencilRef",
         "stencilWriteMask",
-        "stencilFuncMask",
-        "opacity",
-        "polygonOffsetFactor",
-        "polygonOffsetUnits",
+        "stencilFuncMask"
     ];
     for (const prop of requiredNumbers) {
         if (typeof json[prop] !== "number") return false;
     }
 
     const optionalNumbers = [
-        "blendDstAlpha?",
-        "blendEquationAlpha?",
-        "blendSrcAlpha?",
+        "alphaTest",
+        "opacity",
+        "polygonOffsetFactor",
+        "polygonOffsetUnits",
     ];
     for (const prop of optionalNumbers) {
-        if (json[prop] !== undefined) {
-            if (typeof json[prop] !== "number") return false;
-        }
+        if (
+            json[prop] !== undefined &&
+            typeof json[prop] !== "number"
+        ) return false;
     }
 
     const requiredBooleans = [
-        "alphaToCoverage",
-        "clipIntersection",
-        "clipShadows",
         "colorWrite",
         "depthTest",
         "depthWrite",
         "stencilWrite",
+    ];
+    for (const prop of requiredBooleans) {
+        if (typeof json[prop] !== "boolean") return false;
+    }
+
+    const optionalBooleans = [
+        "alphaToCoverage",
         "polygonOffset",
         "premultipliedAlpha",
         "dithering",
@@ -87,11 +91,77 @@ function isMaterial(json: any): json is Material {
         "vertexColors",
         "visible",
     ];
-    for (const prop of requiredBooleans) {
-        if (typeof json[prop] !== "boolean") return false;
+    for (const prop of optionalBooleans) {
+        if (
+            json[prop] !== undefined &&
+            typeof json[prop] !== "boolean"
+        ) return false;
     }
 
-    if (json.userData) {
+    const stencilOps = [
+        THREE.ZeroStencilOp,
+        THREE.KeepStencilOp,
+        THREE.ReplaceStencilOp,
+        THREE.IncrementStencilOp,
+        THREE.DecrementStencilOp,
+        THREE.IncrementWrapStencilOp,
+        THREE.DecrementWrapStencilOp,
+        THREE.InvertStencilOp,
+    ];
+    const requiredEnums = Object.entries({
+        stencilFunc: [
+            THREE.NeverStencilFunc,
+            THREE.LessStencilFunc,
+            THREE.EqualStencilFunc,
+            THREE.LessEqualStencilFunc,
+            THREE.GreaterStencilFunc,
+            THREE.NotEqualStencilFunc,
+            THREE.GreaterEqualStencilFunc,
+            THREE.AlwaysStencilFunc,
+        ],
+        depthFunc: [
+            THREE.NeverDepth,
+            THREE.AlwaysDepth,
+            THREE.LessDepth,
+            THREE.LessEqualDepth,
+            THREE.EqualDepth,
+            THREE.GreaterEqualDepth,
+            THREE.GreaterDepth,
+            THREE.NotEqualDepth,
+        ],
+        stencilFail: stencilOps,
+        stencilZFail: stencilOps,
+        stencilZPass: stencilOps
+    });
+    for (const [prop, options] of requiredEnums) {
+        if (options.includes(json[prop])) return false;
+    }
+
+    const sides = [
+        THREE.FrontSide,
+        THREE.BackSide,
+        THREE.DoubleSide,
+    ];
+    const optionalEnums = Object.entries({
+        blending: [
+            THREE.NoBlending,
+            THREE.NormalBlending,
+            THREE.AdditiveBlending,
+            THREE.SubtractiveBlending,
+            THREE.MultiplyBlending,
+            THREE.CustomBlending,
+        ],
+        side: sides,
+        shadowSide: sides
+    });
+    for (const [prop, options] of optionalEnums) {
+        if (
+            json[prop] !== undefined &&
+            options.includes(json[prop])
+        ) return false;
+    }
+
+    if (json.userData !== undefined) {
         if (!(json.userData instanceof Object)) return false;
     }
 
