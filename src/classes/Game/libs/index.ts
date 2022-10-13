2c2fb4c55b47e4e2741fada9c46158af9a6397d9
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-13 15:24:24 -0300

    add(Game/libs): add materials lib

--- src/classes/Game/libs/index.ts
+++ src/classes/Game/libs/index.ts
@@ -1,4 +1,5 @@
 export { default as cameras } from "./cameras.object";
 export { default as geometries } from "./geometries.object";
 export { default as lights } from "./lights.object";
+export { default as materials } from "./materials.object";
 export { default as objects3D } from "./objects3D.object";
---
+++ src/classes/Game/libs/materials.object.ts
@@ -0,0 +1,18 @@
+const materials = [
+    "LineBasicMaterial",
+    "LineDashedMaterial",
+    "MeshBasicMaterial",
+    "MeshDepthMaterial",
+    "MeshDistanceMaterial",
+    "MeshLambertMaterial",
+    "MeshMatcapMaterial",
+    "MeshNormalMaterial",
+    "MeshPhongMaterial",
+    "MeshPhysicalMaterial",
+    "MeshStandardMaterial",
+    "MeshToonMaterial",
+    "PointsMaterial",
+    "SpriteMaterial",
+] as const;
+
+export default materials;
