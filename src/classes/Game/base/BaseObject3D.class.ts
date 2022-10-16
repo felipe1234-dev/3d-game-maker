2213b99def8d411224b30a591106ef798e3a622d
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-15 22:31:53 -0300

    add(Game/models): add readonly property "type" - that is the constructor name

--- src/classes/Game/base/BaseObject3D.class.ts
+++ src/classes/Game/base/BaseObject3D.class.ts
@@ -3,6 +3,8 @@ import { applyObject3DJSON, parseObjectChildren } from "../utils/private";
 import * as THREE from "three";
 
 class BaseObject3D extends THREE.Object3D implements Game.Object3D {
+    public readonly type: "BaseObject3D" = "BaseObject3D";
+
     public override toJSON(meta?: Game.Formats.Meta): Game.Formats.Object3D {
         const newMeta = meta ? {
             geometries: {},
