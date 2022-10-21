e4e23c72e8a3012b0cdefa00d2ea338aa9fca0bb
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-20 23:10:41 -0300

    refactor(Editor/Viewport): refactor

--- src/pages/Editor/Viewport/index.tsx
+++ src/pages/Editor/Viewport/index.tsx
@@ -1,56 +1,77 @@
 import { useEffect, useState } from "react";
 
-import { useEditor, useGame } from "@local/contexts";
+import {
+    useEditor,
+    useGame,
+} from "@local/contexts";
 import { CursorTooltip } from "@local/components";
 import { t } from "@local/i18n";
+import { useUnmount } from "@local/hooks";
 
 function Viewport() {
-    const [hideTooltip, setHideTooltip] = useState(true);
     const [containerEl, setContainerEl] = useState<HTMLElement | null>(null);
+    const [hideTooltip, setHideTooltip] = useState(true);
 
     const editor = useEditor();
     const game = useGame();
 
+    const onResize = () => {
+        editor.camera.aspect =
+            game.renderer.canvas.offsetWidth /
+            game.renderer.canvas.offsetHeight;
+        editor.camera.updateProjectionMatrix();
+    };
+
+    const onPointerMove = () => {
+        const intersections = editor.transformControls.intersects;
+        setHideTooltip(intersections.length < 1);
+    };
+
     useEffect(() => {
-        const container = containerEl;
-
         if (
-            container &&
-            container.innerHTML.length === 0 &&
-            game.currentScene
-        ) {
-            editor.camera.position.set(0, 10, 10);
-            editor.orbitControls.update();
+            !containerEl ||
+            containerEl.innerHTML.length !== 0 ||
+            !game.currentScene
+        ) return;
 
-            game.renderer.container = container;
+        editor.camera.position.set(0, 10, 10);
+        editor.orbitControls.update();
 
-            const onResize = () => {
-                editor.camera.aspect =
-                    game.renderer.canvas.offsetWidth /
-                    game.renderer.canvas.offsetHeight;
-                editor.camera.updateProjectionMatrix();
-            };
+        game.renderer.container = containerEl;
 
-            window.addEventListener("resize", onResize);
-            new ResizeObserver(onResize).observe(container);
+        window.addEventListener(
+            "resize",
+            onResize
+        );
+        new ResizeObserver(onResize).observe(containerEl);
 
-            game.renderer.canvas.addEventListener("pointermove", () => {
-                const intersections = editor.transformControls.intersects;
-                setHideTooltip(intersections.length < 1);
-            });
+        game.renderer.canvas.addEventListener(
+            "pointermove",
+            onPointerMove
+        );
 
-            game.renderer.startAnimation(
-                () => {},
-                game.currentScene,
-                editor.camera
-            );
+        game.renderer.startAnimation(
+            () => { },
+            game.currentScene,
+            editor.camera
+        );
 
-            console.log(editor);
-            console.log(game);
-        }
+        console.log(editor);
+        console.log(game);
     }, [containerEl, editor, game]);
 
-    const intersections = editor.transformControls.intersects || [];
+    useUnmount(() => {
+        window.removeEventListener(
+            "resize",
+            onResize
+        );
+        game.renderer.canvas.removeEventListener(
+            "pointermove",
+            onPointerMove
+        );
+    });
+
+    const intersections = editor.transformControls.intersects;
     const { object } = intersections[0] || {};
 
     return (
