afde8a6580198ab1ecaa06aaf5c2974eb5266ab8
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-22 11:50:24 -0300

    add(functions): create functions to validate email and URLs

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -7,3 +7,5 @@ export { default as rgbToHex } from "./rgbToHex";
 export { default as setProperty } from "./setProperty";
 export { default as stringToAvatar } from "./stringToAvatar";
 export { default as stringToColor } from "./stringToColor";
+export { default as validateURL } from "./validateURL";
+export { default as validateEmail } from "./validateEmail";
---
+++ src/functions/validateEmail.ts
@@ -0,0 +1,6 @@
+function validateEmail(email: string): boolean {
+    const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexp.test(email.toLowerCase());
+}
+
+export default validateEmail;
