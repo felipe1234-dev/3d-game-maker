1052530fb208611fc37cdd515565cbf2861e578d
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-22 12:01:22 -0300

    refactor(api): export modulesas namespaces

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,6 +8,11 @@ import config from "./firebase-config";
 
 const app = initializeApp(config);
 const db = getFirestore(app);
-const storage =getStorage(app);
+const storage = getStorage(app);
 
 export { app, db, storage };
+
+export * as auth from "./auth";
+export * as gallery from "./collections/gallery";
+export * as games from "./collections/games";
+export * as users from "./collections/users";
