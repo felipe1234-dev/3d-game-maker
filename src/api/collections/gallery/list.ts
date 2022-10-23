1d1c4021c8c7cdee88f6941e4c8e95a54cd9ab1e
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-23 17:12:08 -0300

    refactor(api/gallery): more efficient validation

--- src/api/collections/gallery/list.ts
+++ src/api/collections/gallery/list.ts
@@ -6,17 +6,13 @@ import { Media } from "@local/api/models";
 import { generateQuery, toAlert } from "@local/api/functions";
 import { db } from "@local/api";
 
-function list(filter?: Filter): Promise<Media[]> {
+function list(filter: Filter = {}): Promise<Media[]> {
     return new Promise(async (resolve, reject) => {
-        if (!filter) {
-            filter = {};
-        }
-
         try {
             const q = await generateQuery(db, collectionName, filter);
             const resp = await getDocs(q);
             const docs = resp.docs.map(doc => ({ ...doc.data() }));
-            
+
             resolve(docs as Media[]);
         } catch (error) {
             reject(toAlert(error as FirebaseError));
