df6e20eac9a9e313867538ec6f44fc16ada53387
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-24 00:00:35 -0300

    add(__tests__/unit): clear console.logs

--- src/__tests__/unit/classes/Game/formats/base/isObject3D.test.ts
+++ src/__tests__/unit/classes/Game/formats/base/isObject3D.test.ts
@@ -1,14 +1,12 @@
 import { Game } from "@local/classes";
 
 describe("Game.Formats.isObject3D", () => {
-    it("should return true to all objects in the list", () => {
+    test("All objects should implement Game.Formats.Object3D", () => {
         for (const type of Game.Libs.objects3D) {
             if (type === "Scene") continue;
 
             const object = new Game[type]();
             const format = object.toJSON();
-
-            console.log(type, format);
 
             expect(Game.Formats.isObject3D(format)).toBe(true);
         }
