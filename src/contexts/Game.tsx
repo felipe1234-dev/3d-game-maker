ec5100477ca35034eeb27d7080d803e566144769
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-26 21:55:44 -0300

    add(contexts): add support for user games passed by state

--- src/contexts/Game.tsx
+++ src/contexts/Game.tsx
@@ -1,79 +1,80 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {
+    createContext,
+    useContext,
+    useEffect,
+    useState
+} from "react";
 import { useLocation } from "react-router-dom";
 
-import * as THREE from "three";
 import { Game } from "@local/classes";
-import { t } from "@local/i18n";
+import { Game as GameFormat } from "@local/classes/Game/formats";
+import { useAlert } from "@local/contexts";
+import { Alert, RouteState } from "@local/interfaces";
+import { isRouteState } from "@local/functions";
+import { games } from "@local/api";
+
+function replacer(key: string, value: any): any {
+    if (value === "Infinity") {
+        return Infinity;
+    }
+
+    return value;
+}
 
 const GameContext = createContext<Game.Core | undefined>(undefined);
 
 function GameProvider(props: { children: React.ReactNode }) {
     const [game, setGame] = useState<Game.Core>();
-    let { state } = useLocation();
-    state = state || {};
-    const { inputGame } = state as any;
+
+    const alert = useAlert();
+    const { state } = useLocation();
+
+    let routeState: RouteState = {};
+    if (isRouteState(state)) {
+        routeState = state;
+    }
 
     useEffect(() => {
-        if (inputGame instanceof Game.Core) {
-        } else {
-            const greenMaterial = new Game.MeshPhysicalMaterial({
-                color: 0x00ff00,
-            });
-            const brownMaterial = new Game.MeshPhysicalMaterial({
-                color: "#553820",
-            });
+        (async () => {
+            try {
+                const { gameUrl } = routeState;
 
-            const stage1 = new Game.Stage({ name: "Stage 1" });
-            const scene1 = new Game.Scene({ name: "Scene 1" });
+                if (gameUrl) {
+                    const data = await fetch(gameUrl);
+                    const json = await data.text();
+                    const format = JSON.parse(json, replacer) as GameFormat;
+                    const core = Game.Core.fromJSON(format);
 
-            const box = new Game.BoxGeometry(1, 1, 1);
-            const cube = new Game.Mesh(box, greenMaterial);
-            cube.position.y = 0.5;
-            cube.name = "Oliver";
-            scene1.add(cube);
+                    setGame(core);
+                } else {
+                    const snippets = await games.list({
+                        where: [
+                            ["snippet", "==", true]
+                        ],
+                        orderBy: [
+                            ["createdAt", "desc"]
+                        ],
+                    });
 
-            const rect = new Game.BoxGeometry(6, 0.5, 6);
-            const ground = new Game.Mesh(rect, brownMaterial);
-            ground.position.y = -0.2;
-            ground.name = "Ground";
-            scene1.add(ground);
+                    const gameUrl = snippets[0].url;
+                    const data = await fetch(gameUrl);
+                    const json = await data.text();
+                    const format = JSON.parse(json, replacer) as GameFormat;
+                    const core = Game.Core.fromJSON(format);
 
-            const hemisphereLight = new Game.HemisphereLight();
-            hemisphereLight.intensity = 2;
-            hemisphereLight.name = "Hemisphere Light";
-            scene1.add(hemisphereLight);
-
-            const directionalLight = new Game.DirectionalLight();
-            directionalLight.position.set(3.1, 3.1, 2);
-            directionalLight.name = "Directional Light";
-            scene1.add(directionalLight);
-
-            stage1.addScene(scene1);
-
-            const stage2 = new Game.Stage({ name: "Stage 2" });
-            const scene2 = new Game.Scene({ name: "Scene 2" });
-            const ball = new Game.SphereGeometry(1, 50, 50);
-            const sphere = new Game.Mesh(ball, greenMaterial);
-            scene2.add(sphere);
-            stage2.addScene(scene2);
-
-            const camera = new Game.PerspectiveCamera();
-
-            const obj = new Game.Core({
-                name: t("Default game"),
-                description: t("This is a generated game"),
-                scenes: [scene1, scene2],
-                stages: [stage1, stage2],
-                cameras: [camera],
-            });
-
-            obj.renderer.physicallyCorrectLights = true;
-            obj.renderer.shadowMap.enabled = true;
-            obj.renderer.toneMapping = THREE.LinearToneMapping;
-
-            setGame(obj);
-        }
+                    setGame(core);
+                }
+            } catch (error) {
+                const err = error as Alert;
+                alert.setSeverity(err.severity);
+                alert.setMessage(err.message);
+            }
+        })();
     }, []);
+
+    if (!game) {
+        return <></>;
+    }
 
     return (
         <GameContext.Provider value={game}>
