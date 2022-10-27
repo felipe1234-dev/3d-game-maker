850ed37642cf6efded68ed61a6de3eb395ea407f
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-26 22:05:08 -0300

    refactor(Game/utils/private): refactor

--- src/classes/Game/utils/private/parseObjectChildren.function.ts
+++ src/classes/Game/utils/private/parseObjectChildren.function.ts
@@ -7,13 +7,15 @@ function parseObjectChildren(
     object: Game.Object3D,
     json: Game.Formats.Object3D
 ): void {
-    for (const childJson of json.object.children || []) {
+    for (const objectJson of json.object.children || []) {
         let child: Game.Object3D | undefined = undefined;
+
+        const childJson = { ...json, object: objectJson };
 
         for (const type of Game.Libs.objects3D) {
             if (Game.Formats[`is${type}`](childJson)) {
                 // @ts-ignore
-                child = Game[type].fromJSON({ ...json, object: childJson });
+                child = Game[type].fromJSON(childJson);
             }
         }
 
