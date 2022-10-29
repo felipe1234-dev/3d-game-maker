1be5ad55da7d72fa59f18acc1df9a07c530fbdec
Felipe Alves <felipe.alves@taugor.com.br>
2022-10-29 14:02:37 -0300

    add(Home): create component to list games

---
+++ src/pages/Home/GameList/index.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { auth, games } from "@local/api";
+import { Game } from "@local/api/models";
+import { useAlert } from "@local/contexts";
+import { getLang } from "@local/i18n";
+import { Alert } from "@local/interfaces";
+
+function GameList() {
+    const [gameList, setGameList] = useState<Game[]>([]);
+    const alert = useAlert();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const user = await auth.currentUser();
+                if (!user) return;
+
+                const list = await games.list({
+                    where: [
+                        ["createdBy", "==", user.uid]
+                    ]
+                });
+
+                setGameList([...list]);
+            } catch (err) {
+                const error = err as Alert;
+                alert.setSeverity(error.severity);
+                alert.setMessage(error.message);
+            }
+        })();
+    }, []);
+
+    const lang = getLang();
+
+    return (
+        <>
+            {gameList.map(game => (
+                <Link
+                    key={game.uid}
+                    to={`/${lang}/editor`}
+                    state={{
+                        game,
+                        useLoader: true,
+                    }}
+                >
+                    {game.name} - {game.description}
+                </Link>
+            ))}
+        </>
+    );
+}
+
+export default GameList;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import Navbar from "./Navbar";
+import GameList from "./GameList";
 import "@local/styles/pages/HomePage.scss";
 
 function HomePage() {
     return (
         <Box className="HomePage">
             <Navbar />
+            <GameList />
         </Box>
     );
 }
