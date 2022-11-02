eb1c8a63e20dbb231d51c6c147fc569ae723f521
Felipe Alves <felipe.alves@taugor.com.br>
2022-11-02 18:21:29 -0300

    add(Editor/Menubar): add button to go to a page to test the game

--- src/pages/Editor/Menubar/TestGame.tsx
+++ src/pages/Editor/Menubar/TestGame.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import { Games } from "@styled-icons/fluentui-system-regular";
+import { Link } from "react-router-dom";
 
 import { games } from "@local/api";
 import { Helper } from "@local/components";
 import { useMetadata } from "@local/contexts";
-import { t } from "@local/i18n";
+import { t, getLang } from "@local/i18n";
 
 function TestGame() {
     const [disabled, setDisabled] = useState(false);
@@ -24,14 +25,22 @@ function TestGame() {
         })();
     }, [metadata]);
 
-    const helpText = disabled ? "You need to save your game before testing" : undefined;
+    const lang = getLang();
+    const helpText = disabled ? t("You need to save your game before testing") : undefined;
 
     return (
         <Helper text={helpText} placement="bottom" arrow>
             <span>
                 <Button
+                    component={Link}
+                    to={`/${lang}/test/${metadata.uid}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    state={{
+                        useLoader: true,
+                    }}
+
                     startIcon={<Games width={15} />}
-                    onClick={() => { }}
                     disabled={disabled}
                 >
                     {t("Test")}
