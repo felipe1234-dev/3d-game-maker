39f99c3c24fa885fde5b9de41a2a970abe05d6e7
Felipe Alves <felipe.alves@taugor.com.br>
2022-11-05 08:46:48 -0300

    add(Game/models): add more specific definitions for the Camera model

--- src/classes/Game/models/Camera.interface.ts
+++ src/classes/Game/models/Camera.interface.ts
@@ -1,9 +1,16 @@
 import { Game } from "@local/classes";
 import { Object3D, isObject3D } from "./Object3D.interface";
+import * as THREE from "three";
 
 interface Camera extends Object3D {
     type: typeof Game.Libs.cameras[number];
     game?: Game.Core;
+    matrixWorldInverse: THREE.Matrix4;
+    projectionMatrix: THREE.Matrix4;
+    projectionMatrixInverse: THREE.Matrix4;
+    readonly isCamera: true;
+    getWorldDirection(target: THREE.Vector3): THREE.Vector3;
+    updateMatrixWorld(force?: boolean): void;
 }
 
 function isCamera(obj: any): obj is Camera {
@@ -16,6 +23,7 @@ function isCamera(obj: any): obj is Camera {
     ) return false;
 
     if (!isObject3D(obj)) return false;
+    if (!(obj instanceof THREE.Camera)) return false;
 
     return true;
 }
