dfd607715dd1b7fff8e9407b4cad78c8972f3864
Felipe Alves <felipe.alves@taugor.com.br>
2022-11-26 12:37:56 -0300

    add(Game/Libs): create controls lib

---
+++ src/classes/Game/libs/controls.object.ts
@@ -0,0 +1,5 @@
+const controls = [
+    "PointerLockControls"
+] as const;
+
+export default controls;
--- src/classes/Game/libs/index.ts
+++ src/classes/Game/libs/index.ts
@@ -1,4 +1,5 @@
 export { default as cameras } from "./cameras.object";
+export { default as controls } from "./controls.object";
 export { default as geometries } from "./geometries.object";
 export { default as lights } from "./lights.object";
 export { default as materials } from "./materials.object";
