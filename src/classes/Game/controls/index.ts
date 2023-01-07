9b56eb990aa0a000d1b6cdce58ccf4b7be58159c
Felipe Alves <felipe.alves@taugor.com.br>
2023-01-07 00:35:29 -0300

    add(Game/controls): export BaseControls

--- src/classes/Game/controls/index.ts
+++ src/classes/Game/controls/index.ts
@@ -3,6 +3,7 @@ const SecondPerson = 2;
 const ThirdPerson = 3;
 type Person = typeof FirstPerson | typeof SecondPerson | typeof ThirdPerson;
 
+export { default as BaseControls } from "./BaseControls.class";
 export { default as PointerLockControls } from "./PointerLockControls.class";
 export { default as ClassicalControls } from "./ClassicalControls.class";
 export { default as RotationControls } from "./RotationControls.class";
