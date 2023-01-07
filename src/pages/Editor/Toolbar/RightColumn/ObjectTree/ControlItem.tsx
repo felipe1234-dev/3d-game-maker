e4462dd177d62d1a6afda6dcd085da1f98d78ac5
Felipe Alves <felipe.alves@taugor.com.br>
2023-01-07 00:37:28 -0300

    fix(ObjectTree): not rerendering on delete

--- src/pages/Editor/Toolbar/RightColumn/ObjectTree/ControlItem.tsx
+++ src/pages/Editor/Toolbar/RightColumn/ObjectTree/ControlItem.tsx
@@ -79,6 +79,7 @@ function ControlItem(props: ControlItemProps) {
         game.current.scene.removeControls(control);
 
         evt.stopPropagation();
+        rerenderTreeList();
     };
 
     useEffect(() => {
